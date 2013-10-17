0b469715e833f4a2ca74f74fee8bbe6f92cd83fe
Chtoucas <chtoucas@narvalo.org>
2013-10-17 17:05:55 +0200

    Moved to the new NuGet way to restore packages.

--- NuPack.cmd
+++ NuPack.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-.nuget\NuGet.exe pack src\Narvalo\Narvalo.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Exclude AuthorNotes.txt -Symbols
-.nuget\NuGet.exe pack src\Narvalo.Web\Narvalo.Web.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Symbols
-.nuget\NuGet.exe pack src\Narvalo.Build\Narvalo.Build.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Exclude AuthorNotes.txt -Symbols
+.\tools\NuGet.exe pack src\Narvalo.Build\Narvalo.Build.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Exclude AuthorNotes.md -Symbols
+.\tools\NuGet.exe pack src\Narvalo\Narvalo.csproj -Build -Prop Configuration=Release -OutputDirectory _build
+.\tools\NuGet.exe pack src\Narvalo.Web\Narvalo.Web.csproj -Build -Prop Configuration=Release -OutputDirectory _build
--- etc/AssemblyInfos/AssemblyInfo.Narvalo.Version.cs
+++ etc/AssemblyInfos/AssemblyInfo.Narvalo.Version.cs
@@ -23,8 +23,8 @@
 //#endif
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
 // Version utilisée par NuGet.
-[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0.1")]
