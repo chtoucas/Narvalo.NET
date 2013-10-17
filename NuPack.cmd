86487cfc0e0198381418508115c8bf5ae2bb79a3
Chtoucas <chtoucas@narvalo.org>
2013-10-17 17:34:48 +0200

    Added the msbuild targets to the NuGet package.

--- NuPack.cmd
+++ NuPack.cmd
@@ -1,5 +1,7 @@
 @echo off
 
-.\tools\NuGet.exe pack src\Narvalo.Build\Narvalo.Build.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Exclude AuthorNotes.md -Symbols
+.\tools\NuGet.exe pack src\Narvalo.Build\Narvalo.Build.csproj -Build -Prop Configuration=Release -OutputDirectory _build -Exclude AuthorNotes.md
 .\tools\NuGet.exe pack src\Narvalo\Narvalo.csproj -Build -Prop Configuration=Release -OutputDirectory _build
+@rem -Symbols
 .\tools\NuGet.exe pack src\Narvalo.Web\Narvalo.Web.csproj -Build -Prop Configuration=Release -OutputDirectory _build
+@rem -Symbols
