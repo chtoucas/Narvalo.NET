4768a07ca3df614e3f124a9a255841fefd53b213
Chtoucas <chtoucas@narvalo.org>
2013-10-20 00:29:19 +0200

    Simplified the NuGet packaging task.

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2013-10-19 14:04 +02:00
+ * Timestamp: 2013-10-20 00:25 +02:00
  */
 
 using System;
@@ -8,8 +8,8 @@ using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.1.6")]
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.6")]
 // Version utilisée par NuGet.
 [assembly: AssemblyInformationalVersion("0.2.1")]
