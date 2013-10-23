4406d5c222639cbbdd57ca13ff07c2deaa1f493f
Chtoucas <chtoucas@narvalo.org>
2013-10-23 15:31:23 +0200

    Documentation.

---
+++ docs/HtmlOptimization.md
@@ -0,0 +1,14 @@
+﻿
+Les classes héritées de PageParserFilter sont inspirées du blog :
+http://omari-o.blogspot.com/2009/09/aspnet-white-space-cleaning-with-no.html
+
+Quelques liens intéressants concernant PageParserFilter
+* http://stackoverflow.com/questions/1480373/generic-inhertied-viewpage-and-new-property
+* http://haacked.com/archive/2006/05/03/PageParser.GetCompiledPageInstanceWeirdnessWhenDebugSetToFalseInWeb.config.aspx
+* http://haacked.com/archive/2009/05/05/page-view-lockdown.aspx
+
+Autres pistes à explorer :
+* http://webmarkupmin.codeplex.com/
+* https://github.com/meleze/Meleze.Web
+* http://about.validator.nu/htmlparser/
+* HTMLAgilityPack
--- src/Narvalo.Web/Optimization/MinifiedMvcWebPageRazorHost.cs
+++ src/Narvalo.Web/Optimization/MinifiedMvcWebPageRazorHost.cs
@@ -6,27 +6,19 @@
     internal class MinifiedMvcWebPageRazorHost : MvcWebPageRazorHost
     {
         public MinifiedMvcWebPageRazorHost(string virtualPath, string physicalPath)
-            : base(virtualPath, physicalPath)
-        {
-        }
+            : base(virtualPath, physicalPath) { }
 
         public override RazorCodeGenerator DecorateCodeGenerator(RazorCodeGenerator incomingCodeGenerator)
         {
             if (incomingCodeGenerator is CSharpRazorCodeGenerator) {
                 return new MinifiedMvcCSharpRazorCodeGenerator(
-                            incomingCodeGenerator.ClassName,
-                            incomingCodeGenerator.RootNamespaceName,
-                            incomingCodeGenerator.SourceFileName,
-                            incomingCodeGenerator.Host);
+                    incomingCodeGenerator.ClassName,
+                    incomingCodeGenerator.RootNamespaceName,
+                    incomingCodeGenerator.SourceFileName,
+                    incomingCodeGenerator.Host);
             }
-            //else if (incomingCodeGenerator is VBRazorCodeGenerator) {
-            //    return new MinifiedMvcVBRazorCodeGenerator(
-            //                incomingCodeGenerator.ClassName,
-            //                incomingCodeGenerator.RootNamespaceName,
-            //                incomingCodeGenerator.SourceFileName,
-            //                incomingCodeGenerator.Host);
-            //}
             else {
+                // On ne prend pas en compte les projets VB.NET.
                 return base.DecorateCodeGenerator(incomingCodeGenerator);
             }
         }
