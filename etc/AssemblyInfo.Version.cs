36b599c2aadec83c914800bc7165199c6ecf029e
Chtoucas <chtoucas@narvalo.org>
2013-12-02 13:06:58 +0100

    Preparing for release 0.3

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2013-12-02 12:14 +01:00
+ * Timestamp: 2013-12-02 12:20 +01:00
  */
 
 using System;
@@ -8,8 +8,8 @@ using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.0.2")]
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.2")]
 // Version utilisée par NuGet.
 [assembly: AssemblyInformationalVersion("0.3.0")]
