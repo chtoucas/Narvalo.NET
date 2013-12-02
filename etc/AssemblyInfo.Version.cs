e612824694435c07d3477ea09d0438ad696a4033
Chtoucas <chtoucas@narvalo.org>
2013-12-02 12:15:33 +0100

    Updated NuGet packages.
    Upgraded to ASP.Net MVC 5.

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2013-10-20 20:32 +02:00
+ * Timestamp: 2013-12-02 12:14 +01:00
  */
 
 using System;
@@ -8,8 +8,8 @@ using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.2.1.9")]
+[assembly: AssemblyVersion("0.3.0.0")]
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.2.1.9")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 // Version utilisée par NuGet.
-[assembly: AssemblyInformationalVersion("0.2.1")]
+[assembly: AssemblyInformationalVersion("0.3.0")]
