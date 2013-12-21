f0ffb90c6a83e609a0f6cee0633d7172e3020dba
Chtoucas <chtoucas@narvalo.org>
2013-12-21 17:38:10 +0100

    New schema.org types.

--- src/Narvalo.Web/Semantic/SchemaOrgType.cs
+++ src/Narvalo.Web/Semantic/SchemaOrgType.cs
@@ -3,7 +3,10 @@
     public static class SchemaOrgType
     {
         public const string AboutPage = "http://schema.org/AboutPage";
+        public const string CollectionPage = "http://schema.org/CollectionPage";
         public const string ContactPage = "http://schema.org/ContactPage";
+        public const string ImageGallery = "http://schema.org/ImageGallery";
+        public const string ItemPage = "http://schema.org/ItemPage";
         public const string WebPage = "http://schema.org/WebPage";
 
         public const string Person = "http://schema.org/Person";
