e4163eb633890ec9c4395ac1769f0fdd1e70c6bb
chtoucas <chtoucas@narvalo.org>
2014-01-18 12:56:00 +0100

    Release 0.9.0.1

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2014-01-18 01:02 +01:00
+ * Timestamp: 2014-01-18 12:54 +01:00
  */
 
 using System;
@@ -8,10 +8,10 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.8.0.1")]
+[assembly: AssemblyVersion("0.9.0.1")]
 
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.8.0.1")]
+[assembly: AssemblyFileVersion("0.9.0.1")]
 
 // Version utilisée par NuGet.
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
