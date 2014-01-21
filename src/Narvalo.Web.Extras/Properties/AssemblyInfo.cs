60cf18bfaf1a59f121e903c288ce1c7dc923db2f
chtoucas <chtoucas@narvalo.org>
2014-01-21 15:26:11 +0100

    New release 0.11.0.

--- src/Narvalo.Web.Extras/Properties/AssemblyInfo.cs
+++ src/Narvalo.Web.Extras/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Narvalo.Web.Extras")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Narvalo.Org Web extra utilities.")]
 
 [assembly: Guid("11aa6f7e-1b5c-43a1-982b-4869464efb2f")]
