0323ad87119dc343b96fa35514fc4cab9355d6f6
chtoucas <chtoucas@narvalo.org>
2014-04-27 21:56:45 +0200

    Added the AllowPartiallyTrustedCallers attribute

--- etc/AssemblyInfo.Library.cs
+++ etc/AssemblyInfo.Library.cs
@@ -1,10 +1,13 @@
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 [assembly: AssemblyProduct("Narvalo.Org Libraries.")]
 
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: InternalsVisibleTo("Narvalo.Facts, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d111d3d81f8e971287763b323df0fb3b122b246eb7ad1ef9f7af2bb83241fc5978c06972b09b4b7e9e68f0be4154ff3cc1e08eb22b98b0914f2b0aac326f2354e73a85e4738db0250b4123d0159b499c834ca7f11f3c717defb790df6d024535aa12d63c31d5f6a68b7f26d12f094547bbb84781e8939f1c64c4c90a3ebe0690")]
