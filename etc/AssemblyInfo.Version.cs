0b2f18422e121de099ad16635c5b010fc7187df3
chtoucas <chtoucas@narvalo.org>
2014-05-13 13:12:11 +0200

    New release of Narvalo.Mvp.* 0.23

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2014-05-11 11:39 +02:00
+ * Timestamp: 2014-05-13 13:10 +02:00
  */
 
 using System;
@@ -8,10 +8,10 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("0.22.0.1")]
+[assembly: AssemblyVersion("0.23.0.2")]
 
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("0.22.0.1")]
+[assembly: AssemblyFileVersion("0.23.0.2")]
 
 // Version utilisée par NuGet.
-[assembly: AssemblyInformationalVersion("0.22.0")]
+[assembly: AssemblyInformationalVersion("0.23.0")]
