b490c43642787959ac30ca8e9e036a91df807394
chtoucas <chtoucas@narvalo.org>
2014-05-15 19:09:02 +0200

    Removed dead code

--- src/Narvalo.Mvp.Web/MvpHttpHandler.cs
+++ src/Narvalo.Mvp.Web/MvpHttpHandler.cs
@@ -6,29 +6,6 @@ namespace Narvalo.Mvp.Web
     using System.Web;
     using Narvalo.Mvp.Web.Internal;
 
-    //public abstract class MvpHttpHandler<TViewModel> : MvpHttpHandler, IView<TViewModel>
-    //{
-    //    TViewModel _model;
-
-    //    protected MvpHttpHandler() : base(true) { }
-
-    //    protected MvpHttpHandler(bool throwIfNoPresenterBound) : base(throwIfNoPresenterBound) { }
-
-    //    public TViewModel Model
-    //    {
-    //        get
-    //        {
-    //            if (_model == null) {
-    //                throw new InvalidOperationException(
-    //                    "The Model property is currently null, however it should have been automatically initialized by the presenter. This most likely indicates that no presenter was bound to the control. Check your presenter bindings.");
-    //            }
-
-    //            return _model;
-    //        }
-    //        set { _model = value; }
-    //    }
-    //}
-
     public abstract class MvpHttpHandler : IHttpHandler, IView
     {
         readonly bool _throwIfNoPresenterBound;
