fb33ee3864dbe8daebdebf3159337145c5153d09
chtoucas <chtoucas@narvalo.org>
2014-11-14 11:08:08 +0100

    Removed Narvalo.Mvp.CommandLine

--- src/Narvalo.Mvp.CommandLine/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Narvalo.Mvp.CommandLine")]
-[assembly: AssemblyDescription("")]
-
-[assembly: Guid("4d27cc49-77c7-467b-98b2-331d784b5324")]
