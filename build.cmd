ab37e99d3c9a461284bf249a9e880d74061140e1
chtoucas <chtoucas@narvalo.org>
2014-11-17 23:48:36 +0100

    Work on the build script

--- build.cmd
+++ build.cmd
@@ -2,14 +2,15 @@
 pushd %~dp0
 setlocal
 
-if exist bin goto build
-mkdir bin
+:RestorePackages
+@REM We must restore the NuGet packages first because our MSBuild depends on them.
+".\tools\NuGet\nuget.exe" restore "Narvalo (Core).sln" "/Verbosity" "quiet"
 
 :Build
 
-REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (installed with VS2013) or later since .NET 4.0
-REM is not supported. Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the
-REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
+@REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (installed with VS2013) or later since .NET 4.0
+@REM is not supported. Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the
+@REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
 set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 
