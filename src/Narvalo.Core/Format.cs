d7d0c78682dad428ab756a4ef4a9906cbc10f2cb
chtoucas <chtoucas@narvalo.org>
2014-11-24 22:48:51 +0100

    Hide Squiggles

--- src/Narvalo.Core/Format.cs
+++ src/Narvalo.Core/Format.cs
@@ -8,31 +8,53 @@ namespace Narvalo
 
     public static class Format
     {
-        public static string CurrentCulture(string format, object arg0, params object[] args)
+        public static string CurrentCulture(string format, params object[] args)
         {
-            Require.NotNull(arg0, "arg0");
             Contract.Requires(format != null);
+            Contract.Requires(args != null);
+            Contract.Ensures(Contract.Result<string>() != null);
 
-            return String.Format(CultureInfo.CurrentCulture, format, args == null ? arg0 : GetArguments_(arg0, args));
+            return String.Format(CultureInfo.CurrentCulture, format, args);
         }
 
-        public static string InvariantCulture(string format, object arg0, params object[] args)
+        public static string InvariantCulture(string format, params object[] args)
         {
-            Require.NotNull(arg0, "arg0");
             Contract.Requires(format != null);
+            Contract.Requires(args != null);
+            Contract.Ensures(Contract.Result<string>() != null);
 
-            return String.Format(CultureInfo.InvariantCulture, format, args == null ? arg0 : GetArguments_(arg0, args));
+            return String.Format(CultureInfo.InvariantCulture, format, args);
         }
 
-        static object[] GetArguments_(object arg0, params object[] args)
-        {
-            Enforce.NotNull(args, "args");
+        ////public static string CurrentCulture(string format, object arg0, params object[] args)
+        ////{
+        ////    Require.NotNull(arg0, "arg0");
+        ////    Enforce.NotNull(format, "format");
 
-            var arguments = new object[1 + args.Length];
-            arguments[0] = arg0;
-            args.CopyTo(arguments, 1);
+        ////    var arguments = args == null ? arg0 : GetArguments_(arg0, args);
 
-            return arguments;
-        }
+        ////    return String.Format(CultureInfo.CurrentCulture, format, arguments);
+        ////}
+
+        ////public static string InvariantCulture(string format, object arg0, params object[] args)
+        ////{
+        ////    Require.NotNull(arg0, "arg0");
+        ////    Enforce.NotNull(format, "format");
+
+        ////    var arguments = args == null ? arg0 : GetArguments_(arg0, args);
+
+        ////    return String.Format(CultureInfo.InvariantCulture, format, arguments);
+        ////}
+
+        ////static object[] GetArguments_(object arg0, params object[] args)
+        ////{
+        ////    Enforce.NotNull(args, "args");
+
+        ////    var arguments = new object[1 + args.Length];
+        ////    arguments[0] = arg0;
+        ////    args.CopyTo(arguments, 1);
+
+        ////    return arguments;
+        ////}
     }
 }
