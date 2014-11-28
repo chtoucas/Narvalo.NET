56e012bb8fed223f56c94e387874f3310a716bcb
chtoucas <chtoucas@narvalo.org>
2014-11-28 18:49:49 +0100

    Added NuGet defs

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
-@echo off
-@pushd %~dp0
-@setlocal
+@ECHO off
+@PUSHD %~dp0
+@SETLOCAL
 
 :Build
 @rem Find the most recent 32bit MSBuild.exe on the system. Require v12.0
@@ -8,33 +8,34 @@
 @rem Also handle x86 operating systems, where %ProgramFiles(x86)% is not
 @rem defined. Always quote the %MSBuild% value when setting the variable
 @rem and never quote %MSBuild% references.
-@set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-@if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+@SET MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+@IF not exist %MSBuild% @SET MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 
-@if "%1" == "" @goto BuildDefaults
-@goto BuildCustom
+@IF "%1" == "" @GOTO BuildDefaults
+@GOTO BuildCustom
 
 :BuildCustom
 %MSBuild% Build.proj /nologo /v:m /fl /flp:logfile=.\build.log;verbosity=detailed;encoding=utf-8 /m /nr:false /t:%*
-@if %ERRORLEVEL% neq 0 @goto Failure
-@goto Success
+@IF %ERRORLEVEL% neq 0 @GOTO Failure
+@GOTO Success
 
 :BuildDefaults
 %MSBuild% Build.proj /nologo /v:m /fl /flp:logfile=.\build.log;verbosity=detailed;encoding=utf-8 /m /nr:false
-@if %ERRORLEVEL% neq 0 @goto Failure
-@goto Success
+@IF %ERRORLEVEL% neq 0 @GOTO Failure
+@GOTO Success
 
 :Failure
-@echo.
-@echo *** BUILD FAILED ***
-@goto End
+@ECHO.
+@ECHO *** BUILD FAILED ***
+@ECHO.
+@POPD
+@ENDLOCAL
+@EXIT /B 1
 
 :Success
-@echo.
-@echo *** BUILD SUCCESSFUL ***
-@goto end
-
-:End
-@echo.
-@popd
-@endlocal
+@ECHO.
+@ECHO *** BUILD SUCCESSFUL ***
+@ECHO.
+@POPD
+@ENDLOCAL
+@EXIT /B 0
