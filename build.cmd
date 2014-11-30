f808fc4f25b679b5130dffb533c36956b1a44092
chtoucas <chtoucas@narvalo.org>
2014-11-30 10:27:09 +0100

    Remove the Verbose and strengthen build.cmd
    Instead of using a Verbose switch for the MSBuild projects we simply
    default to high importance level for messages. The caller (cmd) will
    handle the verbosity

--- build.cmd
+++ build.cmd
@@ -1,41 +1,91 @@
-@ECHO off
-@PUSHD %~dp0
-@SETLOCAL
+@echo off
+:: @cls
+@pushd %~dp0
+@setlocal
+
+@set BuildFile=.\Build.proj
+@set LogFile=.\build.log
+
+@echo.
+
+:InitEnvironment
+:: Find the most recent 32bit MSBuild.exe on the system. Require v12.0
+:: (installed with VS2013) or later.
+:: Also handle x86 operating systems, where %ProgramFiles(x86)% is not
+:: defined. Always quote the %MSBuild% value when setting the variable
+:: and never quote %MSBuild% references.
+@set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+@if not exist %MSBuild% (
+    @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+)
+@if not exist %MSBuild% (
+    :: Use VS2012 environment tools?
+    :: @call "%VS120COMNTOOLS%vsvars32.bat"
+    :: Use the one installed with .NET framework?
+    :: @set MSBuild="%windir%\microsoft.net\framework\v4.0.30319\MSBuild"
+    @goto Fatal
+)
+
+:: MSBuild command line with options.
+:: see http://msdn.microsoft.com/en-us/library/vstudio/ms164311%28v=vs.120%29.aspx
+:: /v:m             -> Default logger only displays minimal information.
+:: /fl /flp:logfile -> Detailed log saved in %LogFile%.
+:: /m               -> Use up to the number of processors in the computer.
+:: /nr:false        -> Disable the re-use of MSBuild nodes.
+@set MSBuildCommand=%MSBuild% %BuildFile% /nologo /v:m /fl /flp:logfile=%LogFile%;verbosity=detailed;encoding=utf-8 /m /nr:false
 
 :Build
-@rem Find the most recent 32bit MSBuild.exe on the system. Require v12.0
-@rem (installed with VS2013) or later since .NET 4.0 is not supported.
-@rem Also handle x86 operating systems, where %ProgramFiles(x86)% is not
-@rem defined. Always quote the %MSBuild% value when setting the variable
-@rem and never quote %MSBuild% references.
-@SET MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-@IF not exist %MSBuild% @SET MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+@if "%1"=="" ( @goto BuildDefaults )
 
-@IF "%1" == "" @GOTO BuildDefaults
-@GOTO BuildCustom
+@set Configuration=%1
+@set Target=Default
+@set Production=false
 
-:BuildCustom
-%MSBuild% Build.proj /nologo /v:m /fl /flp:logfile=.\build.log;verbosity=detailed;encoding=utf-8 /m /nr:false /t:%* /p:Production=true;Configuration=Release
-@IF %ERRORLEVEL% neq 0 @GOTO Failure
-@GOTO Success
+@if "%2"=="" ( @goto BuildCustom )
+@set Target=%2
+
+@if "%3"=="" ( @goto BuildCustom )
+@set Production=%3
+
+@goto BuildCustom
 
 :BuildDefaults
-%MSBuild% Build.proj /nologo /v:m /fl /flp:logfile=.\build.log;verbosity=detailed;encoding=utf-8 /m /nr:false /p:Production=true;Configuration=Release
-@IF %ERRORLEVEL% neq 0 @GOTO Failure
-@GOTO Success
+@echo Running Default Build
+@echo.
+
+%MSBuildCommand%
+@if %ERRORLEVEL% neq 0 ( @goto Failure )
+
+@goto Success
+
+:BuildCustom
+@echo Running Custom Build: Configuration=%Configuration% Target=%Target% Production=%Production%
+@echo.
+
+%MSBuildCommand% /t:%Target% /p:Production=%Production%;Configuration=%Configuration%
+@if %ERRORLEVEL% neq 0 ( @goto Failure )
+
+@goto Success
+
+:Fatal
+@echo.
+@echo *** Unable to find a suitable version of MSBuild ***
+@echo.
+@goto :eof
 
 :Failure
-@ECHO.
-@ECHO *** BUILD FAILED ***
-@ECHO.
-@POPD
-@ENDLOCAL
-@EXIT /B 1
+@echo.
+@echo *** Build failed ***
+@goto End
 
 :Success
-@ECHO.
-@ECHO *** BUILD SUCCESSFUL ***
-@ECHO.
-@POPD
-@ENDLOCAL
-@EXIT /B 0
+@echo.
+@echo Congratulations, Build successful!
+@goto End
+
+:End
+@move /y %LogFile% work\ > nul 2>&1
+@echo.
+@popd
+@endlocal
+@exit /B %ERRORLEVEL%
