e2b0abcb031cc79a039e9bb784601a7103d86b54
chtoucas <chtoucas@narvalo.org>
2014-12-02 16:38:46 +0100

    Comments

--- etc/AssemblyInfo.Common.cs
+++ etc/AssemblyInfo.Common.cs
@@ -25,20 +25,22 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-// Versioning objectives:
-// - AssemblyVersion, the one used by the CLR should be of the form
+// Versioning description:
+// - AssemblyVersion, the one used by the CLR.
 //   MAJOR.MINOR.PATCH.0
-// - AssemblyFileVersion, auto-generated, only used to make sure we have
-//   a unique version per build.
+// - AssemblyFileVersion, used to uniquely identify a build.
 //   MAJOR.MINOR.BUILD.0
-// - AssemblyInformationalVersion, public face of the assembly. We will use
-//   it for NuGet package versioning.
-//   MAJOR.MINOR.PATCH(-XXX)
+// - AssemblyInformationalVersion, public face of the assembly. In most cases
+//   this is the version we shall use for NuGet package versioning.
+//   MAJOR.MINOR.PATCH(-PreRelaseLabel)
 //
-// Visual Studio Build:
-//   Inside VS, we don't mind if the versions do not change between builds.
-// Continuous Build:
-// Production Build:
+// MAJOR, MINOR, PATCH and PreRelaseLabel (alpha, beta) are manually set. 
+//
+// BUILD is generated automatically:
+//   Visual Studio build:
+//     Inside VS, we don't mind if the versions do not change between builds.
+//   Continuous build or publicly released build:
+//     BUILD is incremented.
 //
 // NARVALO_CORE and NARVALO_MVP are defined in the csproj.
 #if NARVALO_CORE && NARVALO_MVP
@@ -46,15 +48,11 @@ using System.Runtime.InteropServices;
 #elif NARVALO_CORE
 
 [assembly: AssemblyProduct("Narvalo.Org Core Libraries.")]
-
-#if CONTINUOUS_BUILD
-[assembly: AssemblyVersion("0.26.*")]
-[assembly: AssemblyFileVersion("0.26.0.0")]
-[assembly: AssemblyInformationalVersion("0.26.0-alpha")]
-#else
 [assembly: AssemblyVersion("0.26.0.0")]
-[assembly: AssemblyFileVersion("0.26.0.0")]
 [assembly: AssemblyInformationalVersion("0.26.0-alpha")]
+
+#if !CONTINUOUS_BUILD
+[assembly: AssemblyFileVersion("0.26.0.0")]
 #endif
 
 #if !NO_INTERNALS_VISIBLE_TO
@@ -64,10 +62,12 @@ using System.Runtime.InteropServices;
 #elif NARVALO_MVP
 
 [assembly: AssemblyProduct("Narvalo.Org MVP Libraries.")]
-
 [assembly: AssemblyVersion("0.99.0.0")]
-[assembly: AssemblyFileVersion("0.99.0.0")]
 [assembly: AssemblyInformationalVersion("0.99.0-beta")]
+
+#if !CONTINUOUS_BUILD
+[assembly: AssemblyFileVersion("0.99.0.0")]
+#endif
 
 #if !NO_INTERNALS_VISIBLE_TO
 [assembly: InternalsVisibleTo("Narvalo.Mvp.Facts, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d111d3d81f8e971287763b323df0fb3b122b246eb7ad1ef9f7af2bb83241fc5978c06972b09b4b7e9e68f0be4154ff3cc1e08eb22b98b0914f2b0aac326f2354e73a85e4738db0250b4123d0159b499c834ca7f11f3c717defb790df6d024535aa12d63c31d5f6a68b7f26d12f094547bbb84781e8939f1c64c4c90a3ebe0690")]
