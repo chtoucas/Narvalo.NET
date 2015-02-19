9da4618fcf0f69d6d6ce5491fbd3067840305261
chtoucas <chtoucas@narvalo.org>
2015-02-19 15:10:38 +0100

    Miscs

--- src/Narvalo.Common/Xml/XmlReader$.cs
+++ src/Narvalo.Common/Xml/XmlReader$.cs
@@ -5,7 +5,6 @@ namespace Narvalo.Xml
     using System;
     using System.Xml;
     using System.Xml.Linq;
-    using Narvalo;
 
     /// <summary>
     /// Provides extension methods for <see cref="System.Xml.XmlReader"/>.
@@ -13,114 +12,6 @@ namespace Narvalo.Xml
     public static class XmlReaderExtensions
     {
         #region Methods that simply wrap XmlReader methods for use with an XName parameter.
-
-        public static string GetAttribute(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.GetAttribute(name.LocalName, name.NamespaceName);
-        }
-
-        public static bool IsStartElement(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.IsStartElement(name.LocalName, name.NamespaceName);
-        }
-
-        public static bool MoveToAttribute(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.MoveToAttribute(name.LocalName, name.NamespaceName);
-        }
-
-        public static object ReadElementContentAs(
-            this XmlReader @this,
-            Type returnType,
-            IXmlNamespaceResolver namespaceResolver,
-            XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAs(returnType, namespaceResolver, name.LocalName, name.NamespaceName);
-        }
-
-        public static bool ReadElementContentAsBoolean(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsBoolean(name.LocalName, name.NamespaceName);
-        }
-
-        public static DateTime ReadElementContentAsDateTime(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsDateTime(name.LocalName, name.NamespaceName);
-        }
-
-        public static decimal ReadElementContentAsDecimal(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsDecimal(name.LocalName, name.NamespaceName);
-        }
-
-        public static double ReadElementContentAsDouble(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsDouble(name.LocalName, name.NamespaceName);
-        }
-
-        public static int ReadElementContentAsInt32(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsInt(name.LocalName, name.NamespaceName);
-        }
-
-        public static long ReadElementContentAsInt64(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsLong(name.LocalName, name.NamespaceName);
-        }
-
-        public static object ReadElementContentAsObject(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsObject(name.LocalName, name.NamespaceName);
-        }
-
-        public static float ReadElementContentAsSingle(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsFloat(name.LocalName, name.NamespaceName);
-        }
-
-        public static string ReadElementContentAsString(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadElementContentAsString(name.LocalName, name.NamespaceName);
-        }
 
         public static string ReadElementString(this XmlReader @this, XName name)
         {
@@ -130,36 +21,12 @@ namespace Narvalo.Xml
             return @this.ReadElementString(name.LocalName, name.NamespaceName);
         }
 
-        public static void ReadStartElement(this XmlReader @this, XName name)
+        public static DateTime ReadElementContentAsDateTime(this XmlReader @this, XName name)
         {
             Require.Object(@this);
             Require.NotNull(name, "name");
 
-            @this.ReadStartElement(name.LocalName, name.NamespaceName);
-        }
-
-        public static bool ReadToDescendant(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadToDescendant(name.LocalName, name.NamespaceName);
-        }
-
-        public static bool ReadToFollowing(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadToFollowing(name.LocalName, name.NamespaceName);
-        }
-
-        public static bool ReadToNextSibling(this XmlReader @this, XName name)
-        {
-            Require.Object(@this);
-            Require.NotNull(name, "name");
-
-            return @this.ReadToNextSibling(name.LocalName, name.NamespaceName);
+            return @this.ReadElementContentAsDateTime(name.LocalName, name.NamespaceName);
         }
 
         #endregion
