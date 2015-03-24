112c5fa2129dff7d9c2475caaa72ad131adc37da
chtoucas <chtoucas@narvalo.org>
2015-03-24 16:03:30 +0100

    Added unsafe extension methods for SqlCommand and SqlParameterCollection

--- src/Narvalo.Common/Data/SqlCommand$.cs
+++ src/Narvalo.Common/Data/SqlCommand$.cs
@@ -4,22 +4,114 @@ namespace Narvalo.Data
 {
     using System.Data;
     using System.Data.SqlClient;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Contracts;
 
     using Narvalo.Internal;
 
     /// <summary>
-    /// Provides extension methods for <see cref="System.Data.SqlClient.SqlCommand"/>.
+    /// Provides extension methods for <see cref="SqlCommand"/>.
     /// </summary>
     public static class SqlCommandExtensions
     {
         public static void AddParameter(
-            this SqlCommand @this, string parameterName, SqlDbType parameterType, object value)
+            this SqlCommand @this,
+            string parameterName,
+            SqlDbType parameterType,
+            object value)
         {
             Require.Object(@this);
 
-            @this.Parameters.AssumeNotNull()
-                .Add(parameterName, parameterType).AssumeNotNull()
-                .Value = value;
+            @this.Parameters.AssumeNotNull().AddParameterUnsafe(parameterName, parameterType, value);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterUnsafe(
+            this SqlCommand @this, 
+            string parameterName,
+            SqlDbType parameterType,
+            object value)
+        {
+            Contract.Requires(@this != null);
+
+            @this.Parameters.AssumeNotNull().AddParameterUnsafe(parameterName, parameterType, value);
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlCommand @this, 
+            string parameterName,
+            SqlDbType parameterType,
+            T? value)
+            where T : struct
+        {
+            Require.Object(@this);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlCommand @this, 
+            string parameterName,
+            SqlDbType parameterType,
+            T? value)
+            where T : struct
+        {
+            Contract.Requires(@this != null);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value);
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlCommand @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value)
+        {
+            Require.Object(@this);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value, value != null);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlCommand @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value)
+        {
+            Contract.Requires(@this != null);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value, value != null);
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlCommand @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value,
+            bool condition)
+        {
+            Require.Object(@this);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value, condition);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlCommand @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value,
+            bool condition)
+        {
+            Contract.Requires(@this != null);
+
+            @this.Parameters.AssumeNotNull().AddParameterOrNullUnsafe(parameterName, parameterType, value, condition);
         }
     }
 }
--- src/Narvalo.Common/Data/SqlParameterCollection$.cs
+++ src/Narvalo.Common/Data/SqlParameterCollection$.cs
@@ -2,22 +2,133 @@
 
 namespace Narvalo.Data
 {
+    using System;
     using System.Data;
     using System.Data.SqlClient;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Contracts;
 
     using Narvalo.Internal;
 
     /// <summary>
-    /// Provides extension methods for <see cref="System.Data.SqlClient.SqlParameterCollection"/>.
+    /// Provides extension methods for <see cref="SqlParameterCollection"/>.
     /// </summary>
     public static class SqlParameterCollectionExtensions
     {
         public static void AddParameter(
-            this SqlParameterCollection @this, string parameterName, SqlDbType parameterType, object value)
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            object value)
         {
             Require.Object(@this);
 
+            AddParameterUnsafe(@this, parameterName, parameterType, value);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterUnsafe(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            object value)
+        {
+            Contract.Requires(@this != null);
+
             @this.Add(parameterName, parameterType).AssumeNotNull().Value = value;
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T? value)
+            where T : struct
+        {
+            Require.Object(@this);
+
+            AddParameterOrNullUnsafe(@this, parameterName, parameterType, value);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T? value)
+            where T : struct
+        {
+            Contract.Requires(@this != null);
+
+            if (value.HasValue)
+            {
+                @this.Add(parameterName, parameterType).AssumeNotNull().Value = value.Value;
+            }
+            else
+            {
+                @this.Add(parameterName, parameterType).AssumeNotNull().Value = DBNull.Value;
+            }
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value)
+            where T : class
+        {
+            Require.Object(@this);
+
+            AddParameterOrNullUnsafe(@this, parameterName, parameterType, value, value != null);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value)
+            where T : class
+        {
+            Contract.Requires(@this != null);
+
+            AddParameterOrNullUnsafe(@this, parameterName, parameterType, value, value != null);
+        }
+
+        public static void AddParameterOrNull<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value,
+            bool condition)
+        {
+            Require.Object(@this);
+
+            AddParameterOrNullUnsafe(@this, parameterName, parameterType, value, condition);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", MessageId = "0",
+            Justification = "[Intentionally] This method clearly states that the responsibility for null-checks is on the callers.")]
+        public static void AddParameterOrNullUnsafe<T>(
+            this SqlParameterCollection @this,
+            string parameterName,
+            SqlDbType parameterType,
+            T value,
+            bool condition)
+        {
+            Contract.Requires(@this != null);
+
+            if (condition)
+            {
+                @this.Add(parameterName, parameterType).AssumeNotNull().Value = value;
+            }
+            else
+            {
+                @this.Add(parameterName, parameterType).AssumeNotNull().Value = DBNull.Value;
+            }
         }
     }
 }
