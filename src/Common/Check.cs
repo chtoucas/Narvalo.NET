e4b093bd29052c4316c9c48bbd610e6807d14e23
chtoucas <chtoucas@narvalo.org>
2015-03-25 22:54:33 +0100

    Updated a few copyright that I missed last time

--- src/Common/Check.cs
+++ src/Common/Check.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (c) 2014, Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
 
-namespace Narvalo.Internal
+namespace Narvalo
 {
     using System;
     using System.Diagnostics;
