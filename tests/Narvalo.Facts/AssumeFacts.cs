df7e176fbc017c85c067233c45e108da36146eb9
chtoucas <chtoucas@narvalo.org>
2015-03-27 17:05:08 +0100

    Added test for Assume

---
+++ tests/Narvalo.Facts/AssumeFacts.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo
+{
+    using System;
+
+    using Xunit;
+
+    public static class AssumeFacts
+    {
+        #region AssumeNotNull()
+
+        [Fact]
+        public static void AssumeNotNull_ReturnsBackTheObject()
+        {
+            // Arrange
+            var obj = new Object();
+
+            // Act
+            var result = obj.AssumeNotNull();
+
+            // Assert
+            Assert.True(ReferenceEquals(obj, result));
+        }
+
+        #endregion
+    }
+}
