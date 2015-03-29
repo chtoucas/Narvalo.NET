4c5d4f74f2a5570ba80f72f6e2d1e99eb48e0892
chtoucas <chtoucas@narvalo.org>
2015-03-29 14:43:56 +0200

    Created tests for Either and Switch

---
+++ tests/Narvalo.Facts/Fx/EitherFacts.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Fx
+{
+    using System;
+
+    using Xunit;
+
+    public static class EitherFacts
+    {
+        #region LeftOrNone
+
+        [Fact]
+        public static void LeftOrNone_ReturnsNone_ForRightEither()
+        {
+            // Arrange
+            var either = Either.Right<string, string>("leftValue");
+
+            // Act
+            var result = either.LeftOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void LeftOrNone_ReturnsValue_ForLeftEither()
+        {
+            // Arrange
+            var leftValue = "leftValue";
+            var either = Either.Left<string, string>(leftValue);
+            var expectedResult = Maybe.Of(leftValue);
+
+            // Act
+            var result = either.LeftOrNone();
+
+            // Act & Assert
+            Assert.True(result.IsSome);
+            Assert.Equal(expectedResult, result);
+        }
+
+        #endregion
+
+        #region RightOrNone
+
+        [Fact]
+        public static void RightOrNone_ReturnsNone_ForLeftEither()
+        {
+            // Arrange
+            var either = Either.Left<string, string>("rightValue");
+
+            // Act
+            var result = either.RightOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void RightOrNone_ReturnsValue_ForRightEither()
+        {
+            // Arrange
+            var rightValue = "rightValue";
+            var either = Either.Right<string, string>(rightValue);
+            var expectedResult = Maybe.Of(rightValue);
+
+            // Act
+            var result = either.RightOrNone();
+
+            // Act & Assert
+            Assert.True(result.IsSome);
+            Assert.Equal(expectedResult, result);
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Narvalo.Facts/Fx/SwitchFacts.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Fx
+{
+    using System;
+
+    using Xunit;
+
+    public static class SwitchFacts
+    {
+        #region LeftOrNone
+
+        [Fact]
+        public static void LeftOrNone_ReturnsNone_ForEmptySwitch()
+        {
+            // Arrange
+            var sw = Switch<string, string>.Empty;
+
+            // Act
+            var result = sw.LeftOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void LeftOrNone_ReturnsNone_ForRightSwitch()
+        {
+            // Arrange
+            var sw = Switch.Right<string, string>("leftValue");
+
+            // Act
+            var result = sw.LeftOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void LeftOrNone_ReturnsValue_ForLeftSwitch()
+        {
+            // Arrange
+            var leftValue = "leftValue";
+            var sw = Switch.Left<string, string>(leftValue);
+            var expectedResult = Maybe.Of(leftValue);
+
+            // Act
+            var result = sw.LeftOrNone();
+
+            // Act & Assert
+            Assert.True(result.IsSome);
+            Assert.Equal(expectedResult, result);
+        }
+
+        #endregion
+
+        #region RightOrNone
+
+        [Fact]
+        public static void RightOrNone_ReturnsNone_ForEmptySwitch()
+        {
+            // Arrange
+            var sw = Switch<string, string>.Empty;
+
+            // Act
+            var result = sw.RightOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void RightOrNone_ReturnsNone_ForLeftSwitch()
+        {
+            // Arrange
+            var sw = Switch.Left<string, string>("rightValue");
+
+            // Act
+            var result = sw.RightOrNone();
+
+            // Act & Assert
+            Assert.False(result.IsSome);
+        }
+
+        [Fact]
+        public static void RightOrNone_ReturnsValue_ForRightSwitch()
+        {
+            // Arrange
+            var rightValue = "rightValue";
+            var sw = Switch.Right<string, string>(rightValue);
+            var expectedResult = Maybe.Of(rightValue);
+
+            // Act
+            var result = sw.RightOrNone();
+
+            // Act & Assert
+            Assert.True(result.IsSome);
+            Assert.Equal(expectedResult, result);
+        }
+
+        #endregion
+    }
+}
