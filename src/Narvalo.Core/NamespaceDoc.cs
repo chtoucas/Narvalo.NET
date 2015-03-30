104c4306cd3f6aedf8ed02e0ebe98d992ad53217
chtoucas <chtoucas@narvalo.org>
2015-03-30 22:19:55 +0200

    Missing one important comment

--- src/Narvalo.Core/NamespaceDoc.cs
+++ src/Narvalo.Core/NamespaceDoc.cs
@@ -5,28 +5,28 @@ namespace Narvalo
     using System.Runtime.CompilerServices;
 
     /// <summary>
-    /// The Narvalo namespace contains classes dealing with Code Contracts 
+    /// The Narvalo namespace contains classes dealing with Code Contracts
     /// and argument validation.
     /// </summary>
     /// <remarks>
     /// <para>We provide four static classes to perform argument validation:
     /// <list type="bullet">
     /// <item><see cref="Enforce"/> throws <see cref="System.ArgumentException"/>.</item>
-    /// <item><see cref="Require"/> uses Code Contracts preconditions 
+    /// <item><see cref="Require"/> uses Code Contracts preconditions
     /// and throws <see cref="System.ArgumentException"/>.</item>
-    /// <item><see cref="Promise"/> uses Code Contracts preconditions 
+    /// <item><see cref="Promise"/> uses Code Contracts preconditions
     /// and throws unrecoverable exceptions in debug builds.</item>
     /// <item><see cref="Check"/> throws unrecoverable exceptions in debug builds.</item>
     /// </list>
     /// Each one serves a different purpose but only the first two will survive
     /// in retail builds.</para>
-    /// <para>For parameter validation of public methods, use <see cref="Require"/> 
-    /// and <see cref="Enforce"/> which provides support for conditions not yet handled 
+    /// <para>For parameter validation of public methods, use <see cref="Require"/>
+    /// and <see cref="Enforce"/> which provides support for conditions not yet handled
     /// by Code Contracts.</para>
     /// <para>For parameter validation of private methods, use <see cref="Promise"/>.</para>
-    /// <para>For parameter validation of protected methods in sealed classes,
+    /// <para>For parameter validation of protected overriden methods in sealed classes,
     /// when you know for certain that *ALL* callers satisfy the condition,
-    /// use <see cref="Promise"/> when the base method does not declare any contract; 
+    /// use <see cref="Promise"/> when the base method does not declare any contract;
     /// otherwise use <see cref="Check"/>.</para>
     /// <para>We also provide two static classes to help with Code Contracts:
     /// <list type="bullet">
