7476f22b3d55e339e227f0f29a49dc570b7e2fe2
chtoucas <chtoucas@narvalo.org>
2015-04-15 21:39:57 +0200

    NuGet.Core updates

--- tools/NuGetAgent/Scripts/load-project.fsx
+++ tools/NuGetAgent/Scripts/load-project.fsx
@@ -1,4 +1,5 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
 #load @"load-references.fsx"
 #load @"..\AssemblyInfo.fs"
       @"..\Constants.fs"
--- tools/NuGetAgent/Scripts/load-references.fsx
+++ tools/NuGetAgent/Scripts/load-references.fsx
@@ -1,8 +1,9 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
 #r @"..\..\packages\Microsoft.Web.Xdt.2.1.1\lib\net40\Microsoft.Web.XmlTransform.dll"
-#r @"..\..\packages\NuGet.Core.2.8.3\lib\net40-Client\NuGet.Core.dll"
 #r @"System.dll"
 #r @"System.Core.dll"
 #r @"System.Numerics.dll"
 #r @"System.Xml.Linq.dll"
 #r @"..\..\packages\UnionArgParser.0.8.6\lib\net40\UnionArgParser.dll"
+#r @"..\..\packages\Nuget.Core.2.8.5\lib\net40-Client\NuGet.Core.dll"
