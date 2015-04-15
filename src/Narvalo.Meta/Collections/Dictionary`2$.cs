397b2f9d04da260de0a83f2f52e9551960f6194a
chtoucas <chtoucas@narvalo.org>
2015-04-15 11:31:03 +0200

    New Narvalo.Meta project.

---
+++ src/Narvalo.Meta/Collections/Dictionary`2$.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Collections
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    using Narvalo.Fx;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="IDictionary{T,U}"/> that depend on the <see cref="Maybe{T}"/> class.
+    /// </summary>
+    public static partial class DictionaryExtensions
+    {
+        /// <summary>
+        /// Version sans passage par référence de la méthode <see cref="System.Collections.Generic.IDictionary{T,U}.TryGetValue"/>.
+        /// </summary>
+        /// <typeparam name="TKey">Le type des clés du dictionnaire.</typeparam>
+        /// <typeparam name="TValue">Le type des valeurs du dictionnaire.</typeparam>
+        /// <param name="this">Le dictionnaire à analyser.</param>
+        /// <param name="key">La clé à rechercher.</param>
+        /// <returns>Retourne une monade Maybe contenant la valeur associée à la clé.</returns>
+        public static Maybe<TValue> MayGetValue<TKey, TValue>(
+            this IDictionary<TKey, TValue> @this,
+            TKey key)
+        {
+            Require.Object(@this);
+            Contract.Ensures(Contract.Result<Maybe<TValue>>() != null);
+
+            if (key == null) { return Maybe<TValue>.None; }
+
+            TValue value;
+            return @this.TryGetValue(key, out value) ? Maybe.Of(value) : Maybe<TValue>.None;
+        }
+    }
+}
---
+++ src/Narvalo.Meta/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Narvalo.Common")]
+[assembly: AssemblyDescription("Narvalo Common Library containing mostly extension methods for classes from the BCL and few goodies.")]
+
+[assembly: Guid("ccf49f4b-6df3-48a2-8164-2c6bb4e9c6f5")]
+
+#if !NO_INTERNALS_VISIBLE_TO // Make internals visible to the test projects.
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Narvalo.Facts" + Narvalo.Properties.AssemblyInfo.PublicKeySuffix)]
+#endif
