da6dc74263fb685617991daca9715d216ad0cc4d
chtoucas <chtoucas@narvalo.org>
2015-04-22 10:07:30 +0200

    Added empty project for Narvalo.Core

---
+++ src/Narvalo.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyTitle("Narvalo.Core")]
+[assembly: AssemblyDescription("Narvalo Core Library.")]
+
+[assembly: SecurityTransparent]
+
+#if !NO_INTERNALS_VISIBLE_TO // Make internals visible to the test projects.
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Narvalo.Facts" + Narvalo.Properties.AssemblyInfo.PublicKeySuffix)]
+#endif
