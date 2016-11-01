eb3187d71b3c028060386f07b5722bd011388b60
Chtoucas <chtoucas@narvalo.org>
2016-11-01 07:14:39 +0100

    Narvalo.PowerShell becomes a true PS project.

--- src/Narvalo.PowerShell/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
-
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Narvalo.PowerShell")]
-[assembly: AssemblyDescription("Narvalo PowerShell Modules.")]
-
-[assembly: Guid("ca89ed43-2231-4eac-a63e-b85c45539537")]
