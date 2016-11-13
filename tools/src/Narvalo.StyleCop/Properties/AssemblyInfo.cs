948fe06038095737f6fb930b78058f7ad74e3c35
Chtoucas <chtoucas@narvalo.org>
2016-11-13 21:26:05 +0100

    Removed the Narvalo.StyleCop project.

--- tools/src/Narvalo.StyleCop/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
-
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Narvalo.StyleCop")]
-[assembly: AssemblyDescription("Narvalo Custom StyleCop Rules.")]
-
-[assembly: Guid("d7607d93-37e5-4895-ae7d-058a2fef03d5")]
