6f893b4fc2b9d53ccca45848a5dee7de38c40648
Chtoucas <chtoucas@narvalo.org>
2016-11-15 18:33:04 +0100

    New tests.

--- src/Narvalo.Cerbere/Expect.cs
+++ src/Narvalo.Cerbere/Expect.cs
@@ -6,69 +6,91 @@ namespace Narvalo
     using System.Diagnostics;
     using System.Diagnostics.Contracts;
 
-    [DebuggerStepThrough]
+    /// <summary>
+    /// Provides Code Contracts abbreviators.
+    /// </summary>
     public static class Expect
     {
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void State(bool testCondition) => True(testCondition);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void True(bool testCondition) => Contract.Requires(testCondition);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void Range(bool rangeCondition) => True(rangeCondition);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void NotNull<T>(T value) where T : class => True(value != null);
 
+        /// <summary>
+        /// Checks that the specified object parameter is not <see langword="null"/> or empty.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void NotNullOrEmpty(string value) => True(!String.IsNullOrEmpty(value));
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void NotNullOrWhiteSpace(string value) => True(!String.IsNullOrWhiteSpace(value));
 
+        /// <summary>
+        /// Checks that the specified object parameter is not <see langword="null"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="this"/>.</typeparam>
+        /// <param name="this">The object to check.</param>
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void Object<T>(T @this) where T : class => True(@this != null);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void Property(bool testCondition) => True(testCondition);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void Property<T>(T value) where T : class => True(value != null);
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
             Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
         public static void PropertyNotEmpty(string value) => True(!String.IsNullOrEmpty(value));
 
+        [DebuggerHidden]
         [ContractAbbreviator]
         [Conditional("CONTRACTS_FULL")]
         [ExcludeFromCodeCoverage(
---
+++ tests/Narvalo.Facts/ExpectFacts.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo
+{
+    using System;
+
+    using Xunit;
+
+    public static class ExpectFacts
+    {
+        #region Object()
+
+        [Fact]
+        public static void Object_DoesNotThrow_ForNull()
+        {
+            // Act
+            Expect.Object((string)null);
+        }
+
+        [Fact]
+        public static void Object_DoesNotThrow_ForNonNull()
+        {
+            // Act
+            Expect.Object("this");
+        }
+
+        #endregion
+
+        #region NotNullOrEmpty()
+
+        [Fact]
+        public static void NotNullOrEmpty_DoesNothing()
+        {
+            // Act
+            Expect.NotNullOrEmpty(null);
+            Expect.NotNullOrEmpty(String.Empty);
+            Expect.NotNullOrEmpty("value");
+        }
+
+        #endregion
+
+        #region Object()
+
+        [Fact]
+        public static void Object_DoesNothing()
+        {
+            // Act
+            Expect.Object((string)null);
+            Expect.Object("value");
+        }
+
+        #endregion
+    }
+}
