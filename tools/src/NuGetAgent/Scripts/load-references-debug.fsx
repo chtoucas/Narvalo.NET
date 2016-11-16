8c206953ff227fe5db1106cbf5fe4d8b3cd62f61
Chtoucas <chtoucas@narvalo.org>
2016-11-16 17:44:39 +0100

    Cleanup NuGetAgent project.

--- tools/src/NuGetAgent/Scripts/load-references-debug.fsx
+++ tools/src/NuGetAgent/Scripts/load-references-debug.fsx
@@ -1,10 +1,7 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #r "../../../packages/Microsoft.Web.Xdt.2.1.1/lib/net40/Microsoft.Web.XmlTransform.dll"
-#r "System.Core.dll"
 #r "System.dll"
-#r "System.Numerics.dll"
-#r "System.Xml.Linq.dll"
 #r "../../../packages/UnionArgParser.0.8.6/lib/net40/UnionArgParser.dll"
 #r "../../../packages/NuGet.Core.2.12.0/lib/net40-Client/NuGet.Core.dll"
-#r "Microsoft.CSharp.dll"
+#r "System.Xml.Linq.dll"
