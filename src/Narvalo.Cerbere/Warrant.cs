2a593d3ceae48a994a91a17452fcbea2e71d057c
Chtoucas <chtoucas@narvalo.org>
2016-12-11 10:24:18 +0100

    Add helpers for postconditions.

---
+++ src/Narvalo.Cerbere/Warrant.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo
+{
+    using System.Diagnostics;
+    using System.Diagnostics.Contracts;
+
+    public static class Warrant
+    {
+        [ContractAbbreviator]
+        [Conditional("CONTRACTS_FULL")]
+        [ExcludeFromCodeCoverage(
+            Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
+        public static void IsTrue()
+            => Contract.Ensures(Contract.Result<bool>() == true);
+
+        [ContractAbbreviator]
+        [Conditional("CONTRACTS_FULL")]
+        [ExcludeFromCodeCoverage(
+            Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
+        public static void IsFalse()
+            => Contract.Ensures(Contract.Result<bool>() == false);
+
+        [ContractAbbreviator]
+        [Conditional("CONTRACTS_FULL")]
+        [ExcludeFromCodeCoverage(
+            Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
+        public static void NotNull<T>() where T : class
+            => Contract.Ensures(Contract.Result<T>() != null);
+
+        [ContractAbbreviator]
+        [Conditional("CONTRACTS_FULL")]
+        [ExcludeFromCodeCoverage(
+            Justification = "OpenCover can't discover the tests because of the CONTRACTS_FULL conditional.")]
+        public static void NotNullOrEmpty()
+        {
+            // TODO: Explain why we do not use String.IsNullOrEmpty().
+            Contract.Ensures(Contract.Result<string>() != null);
+            Contract.Ensures(Contract.Result<string>().Length != 0);
+        }
+    }
+}
