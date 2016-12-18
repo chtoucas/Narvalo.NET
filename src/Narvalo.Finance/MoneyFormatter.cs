35a5103d62e1b55e761364c1c162206ca6863ca4
Chtoucas <chtoucas@narvalo.org>
2016-12-18 12:18:43 +0100

    Preliminary work for MoneyFormatter.

--- src/Narvalo.Finance/MoneyFormatter.cs
+++ src/Narvalo.Finance/MoneyFormatter.cs
@@ -5,55 +5,91 @@ namespace Narvalo.Finance
     using System;
     using System.Globalization;
 
-    // See https://msdn.microsoft.com/fr-fr/library/dwhawy9k(v=vs.110).aspx
-    // and https://msdn.microsoft.com/en-us/library/txafckwd(v=vs.110).aspx
-    public sealed class MoneyFormatter : IFormatProvider
+    // See:
+    // - Formatting Types in the .NET Framework
+    //   https://msdn.microsoft.com/en-us/library/26etazsy(v=vs.110).aspx
+    // - How to: Define and Use Custom Numeric Format Providers
+    //  https://msdn.microsoft.com/en-us/library/bb762932(v=vs.110).aspx
+    // - Composite Formatting
+    //  https://msdn.microsoft.com/en-us/library/txafckwd(v=vs.110).aspx
+    // - Standard Numeric Format Strings
+    //  https://msdn.microsoft.com/en-us/library/dwhawy9k(v=vs.110).aspx
+    // http://stackoverflow.com/questions/36005332/custom-currency-format-in-c-sharp
+    public sealed class MoneyFormatter : IFormatProvider, ICustomFormatter
     {
-        private readonly MoneyFormatter_ _formatter = new MoneyFormatter_();
+        private static readonly MoneyFormatter s_Formatter = new MoneyFormatter();
 
+        #region IFormatProvider implementation.
+
+        // Method called by the framework before an object's implementation of IFormattable.ToString(),
+        // whenever we use String.Format() or StringBuilder.AppendFormat().
         public object GetFormat(Type formatType)
-            => formatType == typeof(ICustomFormatter)
-            ? _formatter
-            : CultureInfo.CurrentCulture.GetFormat(formatType);
+            => formatType == typeof(ICustomFormatter) ? s_Formatter : null;
+            // REVIEW: CultureInfo.CurrentCulture.GetFormat(formatType) instead of null?
 
-        private sealed class MoneyFormatter_ : ICustomFormatter
+        #endregion
+
+        #region ICustomFormatter implementation.
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
         {
-            public string Format(string format, object arg, IFormatProvider formatProvider)
+            if (arg == null) { return String.Empty; }
+
+            if (arg.GetType() == typeof(Money) && format == "C")
             {
-                if (arg == null) { return String.Empty; }
-
-                if (arg.GetType() == typeof(Decimal) && format == "C")
-                {
-                    //return Format((Money)arg, formatProvider);
-                    throw new NotImplementedException();
-                }
-                //if (arg.GetType() == typeof(Money<>) && format == "C")
-                //{
-                //    throw new NotImplementedException();
-                //}
-
-                var formattable = arg as IFormattable;
-                return formattable == null
-                    ? arg.ToString()
-                    : formattable.ToString(format, formatProvider);
+                throw new NotImplementedException();
+            }
+            if (arg.GetType() == typeof(Money<>) && format == "C")
+            {
+                throw new NotImplementedException();
             }
 
-            private string Format(Money money, CultureInfo cultureInfo)
+            var formattable = arg as IFormattable;
+            return formattable == null
+                ? arg.ToString()
+                : formattable.ToString(format, formatProvider);
+        }
+
+        #endregion
+
+        public static string Format(decimal amount, Currency currency, CultureInfo cultureInfo)
+        {
+            var ri = new RegionInfo(cultureInfo.Name);
+
+            if (ri.ISOCurrencySymbol == currency.Code)
             {
-                var ri = new RegionInfo(cultureInfo.Name);
-
-                if (ri.ISOCurrencySymbol == money.Currency.Code)
-                {
-                    return money.Amount.ToString("0:C", cultureInfo);
-                }
-                else
-                {
-                    var numberFormat = (NumberFormatInfo)cultureInfo.NumberFormat.Clone();
-                    numberFormat.CurrencySymbol = "XXX";
-
-                    return money.Amount.ToString("0:C", numberFormat);
-                }
+                return amount.ToString("0:C", cultureInfo);
             }
+            else
+            {
+                var nfi = (NumberFormatInfo)cultureInfo.NumberFormat.Clone();
+                nfi.CurrencySymbol = CurrencySymbol.GetFallbackSymbol(currency.Code);
+
+                return amount.ToString("0:C", nfi);
+            }
+        }
+
+        public static string Format(Money money, CultureInfo cultureInfo)
+        {
+            var ri = new RegionInfo(cultureInfo.Name);
+
+            if (ri.ISOCurrencySymbol == money.Currency.Code)
+            {
+                return money.Amount.ToString("0:C", cultureInfo);
+            }
+            else
+            {
+                var nfi = (NumberFormatInfo)cultureInfo.NumberFormat.Clone();
+                nfi.CurrencySymbol = CurrencySymbol.GetFallbackSymbol(money.Currency.Code);
+
+                return money.Amount.ToString("0:C", nfi);
+            }
+        }
+
+        public static string Format<TCurrency>(Money<TCurrency> money, CultureInfo cultureInfo)
+            where TCurrency : Currency
+        {
+            throw new NotImplementedException();
         }
     }
 }
