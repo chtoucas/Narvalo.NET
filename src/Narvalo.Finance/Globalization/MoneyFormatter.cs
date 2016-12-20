6e61dc50bec8699598c315faad6be915477cb6eb
Chtoucas <chtoucas@narvalo.org>
2016-12-20 15:01:51 +0100

    Simplify MoneyFormatter.

--- src/Narvalo.Finance/Globalization/MoneyFormatter.cs
+++ src/Narvalo.Finance/Globalization/MoneyFormatter.cs
@@ -9,7 +9,7 @@ namespace Narvalo.Finance.Globalization
 
     internal sealed class MoneyFormatter
     {
-        private const string DEFAULT_FORMAT = "C";
+        private const string DEFAULT_FORMAT = "G";
 
         public static string Format(Money money, string format, MoneyFormatInfo mfi)
         {
@@ -30,37 +30,14 @@ namespace Narvalo.Finance.Globalization
                 format = DEFAULT_FORMAT;
             }
 
-            // FIXME: Simplify and remove most.
-            // TODO: Improve C2. Add arbitrary formatting "###"? Check length?
-            // TODO: Create a custom formatter using the CLDR data.
-            // http://www.guysmithferrier.com/post/2007/07/Displaying-Currencies-In-Your-Local-Format.aspx
-            // https://codeascraft.com/2016/04/19/how-etsy-formats-currency/
             switch (format)
             {
                 case "G":
                 case "g":
-                    // General: use the currency format "C" for the underlying NumberFormatInfo,
-                    // but replace the currency symbol by the currency code.
                     return FormatGeneral(amount, currency, mfi);
                 case "C":
                 case "c":
-                    // General: use the currency format "C" for the underlying NumberFormatInfo,
-                    // but override the currency symbol if necessary.
-                    return FormatCurrency(amount, currency, "0:C2", mfi);
-                case "C2":
-                case "c2":
-                    // General: use the currency format "C2" for the underlying NumberFormatInfo,
-                    // but override the currency symbol if necessary.
-                    return FormatCurrency(amount, currency, "0:C", mfi);
-                case "L":
-                case "l":
-                    // Legal.
-                    // http://publications.europa.eu/code/en/en-370303.htm
-                    return Narvalo.Format.Current("{1}\u00A0{0:F2}", amount, currency.Code);
-                case "N":
-                case "n":
-                    // Neutral.
-                    return Narvalo.Format.Current("{0:F2} ({1})", amount, currency.Code);
+                    return FormatCurrency(amount, currency, mfi);
                 default:
                     throw new FormatException(
                         Narvalo.Format.Current(Strings.Money_InvalidFormatSpecification));
@@ -73,13 +50,17 @@ namespace Narvalo.Finance.Globalization
             Demand.NotNull(currency);
             Warrant.NotNull<string>();
 
+            // http://publications.europa.eu/code/en/en-370303.htm
+            //return Narvalo.Format.Current("{1}\u00A0{0:F2}", amount, currency.Code);
+            // CurrencyGroupSeparator
+            // CurrencyDecimalSeparator
             var nfi = (NumberFormatInfo)mfi.NumberFormat.Clone();
             nfi.CurrencySymbol = currency.Code;
 
-            return amount.ToString("C", mfi.NumberFormat);
+            return amount.ToString("C", nfi);
         }
 
-        private static string FormatCurrency(decimal amount, Currency currency, string format, MoneyFormatInfo mfi)
+        private static string FormatCurrency(decimal amount, Currency currency, MoneyFormatInfo mfi)
         {
             Require.NotNull(mfi, nameof(mfi));
             Demand.NotNull(currency);
@@ -87,14 +68,14 @@ namespace Narvalo.Finance.Globalization
 
             if (mfi.CurrencyIsNative)
             {
-                return amount.ToString(format, mfi.NumberFormat);
+                return amount.ToString("C", mfi.NumberFormat);
             }
             else
             {
                 var nfi = (NumberFormatInfo)mfi.NumberFormat.Clone();
-                nfi.CurrencySymbol = "\x00a4";  // ¤ - CURRENCY SIGN
+                nfi.CurrencySymbol = currency.Code;
 
-                return amount.ToString(format, mfi.NumberFormat);
+                return amount.ToString("C", nfi);
             }
         }
     }
