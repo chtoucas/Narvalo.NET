3aeca07bfecc338b4840a91895e9b2fb664e6708
Chtoucas <chtoucas@narvalo.org>
2016-12-23 21:51:13 +0100

    Experiment.

--- src/Narvalo.Finance/Globalization/MoneyFormatter.cs
+++ src/Narvalo.Finance/Globalization/MoneyFormatter.cs
@@ -11,6 +11,114 @@ namespace Narvalo.Finance.Globalization
     {
         private const string DEFAULT_FORMAT = "G";
 
+        public static string Format(Money money, string format, CultureInfo ci)
+        {
+            Demand.NotNull(mfi);
+            Warrant.NotNull<string>();
+
+            return Format(money.Amount, money.Currency, format, ci);
+        }
+
+        public static string Format(decimal amount, Currency currency, string format, CultureInfo ci)
+        {
+            Demand.NotNull(currency);
+            Demand.NotNull(mfi);
+            Warrant.NotNull<string>();
+
+            if (format == null || format.Length == 0)
+            {
+                format = DEFAULT_FORMAT;
+            }
+
+            switch (format)
+            {
+                case "C":
+                case "c":
+                    // Currency.
+                    return FormatCurrency(amount, currency, ci);
+                case "A":
+                case "a":
+                    // Amount.
+                    return FormatAmount(amount, currency, ci);
+                case "G":
+                case "g":
+                    // General (default).
+                    return FormatGeneral(amount, currency, ci);
+                default:
+                    throw new FormatException(
+                        Narvalo.Format.Current(Strings.Money_InvalidFormatSpecification));
+            }
+        }
+
+        private static string FormatGeneral(decimal amount, Currency currency, CultureInfo mfi)
+        {
+            Require.NotNull(mfi, nameof(mfi));
+            Demand.NotNull(currency);
+            Warrant.NotNull<string>();
+
+            // http://publications.europa.eu/code/en/en-370303.htm
+            var nfi = CloneNumberFormat(mfi);
+
+            return currency.Code + "\u00A0" + amount.ToString("C", nfi).Trim();
+        }
+
+        private static string FormatAmount(decimal amount, Currency currency, CultureInfo mfi)
+        {
+            Require.NotNull(mfi, nameof(mfi));
+            Demand.NotNull(currency);
+            Warrant.NotNull<string>();
+
+            var nfi = CloneNumberFormat(mfi);
+
+            return amount.ToString("C", nfi).Trim();
+        }
+
+        private static string FormatCurrency(decimal amount, Currency currency, CultureInfo ci)
+        {
+            Require.NotNull(ci, nameof(ci));
+            Demand.NotNull(currency);
+            Warrant.NotNull<string>();
+
+            if (IsCurrencyNative(ci, currency))
+            {
+                return amount.ToString("C", ci.NumberFormat);
+            }
+            else
+            {
+                var nfi = CloneNumberFormat(ci, currency.Code);
+
+                return amount.ToString("C", nfi);
+            }
+        }
+
+        private static NumberFormatInfo CloneNumberFormat(CultureInfo ci)
+            => CloneNumberFormat(ci, String.Empty);
+
+        private static NumberFormatInfo CloneNumberFormat(CultureInfo ci, string currencySymbol)
+        {
+            var nfi = (NumberFormatInfo)ci.NumberFormat.Clone();
+            nfi.CurrencySymbol = currencySymbol;
+
+            return nfi;
+        }
+
+        private static bool IsCurrencyNative(CultureInfo ci, Currency currency)
+        {
+            if (!ci.IsNeutralCulture)
+            {
+                var ri = new RegionInfo(ci.Name);
+
+                if (ri.ISOCurrencySymbol == currency.Code)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #region MoneyFormatInfo
+
         public static string Format(Money money, string format, MoneyFormatInfo mfi)
         {
             Demand.NotNull(mfi);
@@ -32,12 +140,18 @@ namespace Narvalo.Finance.Globalization
 
             switch (format)
             {
-                case "G":
-                case "g":
-                    return FormatGeneral(amount, currency, mfi);
                 case "C":
                 case "c":
+                    // Currency.
                     return FormatCurrency(amount, currency, mfi);
+                case "A":
+                case "a":
+                    // Amount.
+                    return FormatAmount(amount, currency, mfi);
+                case "G":
+                case "g":
+                    // General (default).
+                    return FormatGeneral(amount, currency, mfi);
                 default:
                     throw new FormatException(
                         Narvalo.Format.Current(Strings.Money_InvalidFormatSpecification));
@@ -51,10 +165,20 @@ namespace Narvalo.Finance.Globalization
             Warrant.NotNull<string>();
 
             // http://publications.europa.eu/code/en/en-370303.htm
-            var nfi = (NumberFormatInfo)mfi.NumberFormat.Clone();
-            nfi.CurrencySymbol = String.Empty;
+            var nfi = CloneNumberFormat(mfi);
 
             return currency.Code + "\u00A0" + amount.ToString("C", nfi).Trim();
+        }
+
+        private static string FormatAmount(decimal amount, Currency currency, MoneyFormatInfo mfi)
+        {
+            Require.NotNull(mfi, nameof(mfi));
+            Demand.NotNull(currency);
+            Warrant.NotNull<string>();
+
+            var nfi = CloneNumberFormat(mfi);
+
+            return amount.ToString("C", nfi).Trim();
         }
 
         private static string FormatCurrency(decimal amount, Currency currency, MoneyFormatInfo mfi)
@@ -69,11 +193,23 @@ namespace Narvalo.Finance.Globalization
             }
             else
             {
-                var nfi = (NumberFormatInfo)mfi.NumberFormat.Clone();
-                nfi.CurrencySymbol = currency.Code;
+                var nfi = CloneNumberFormat(mfi, currency.Code);
 
                 return amount.ToString("C", nfi);
             }
         }
+
+        private static NumberFormatInfo CloneNumberFormat(MoneyFormatInfo mfi)
+            => CloneNumberFormat(mfi, String.Empty);
+
+        private static NumberFormatInfo CloneNumberFormat(MoneyFormatInfo mfi, string currencySymbol)
+        {
+            var nfi = (NumberFormatInfo)mfi.NumberFormat.Clone();
+            nfi.CurrencySymbol = currencySymbol;
+
+            return nfi;
+        }
+
+        #endregion
     }
 }
