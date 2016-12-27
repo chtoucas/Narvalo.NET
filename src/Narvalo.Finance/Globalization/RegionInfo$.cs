b39f3bb9ecceb787c2b026a6a7162e38232f4d10
Chtoucas <chtoucas@narvalo.org>
2016-12-27 18:00:17 +0100

    Add two extensions for RegionInfo.

---
+++ src/Narvalo.Finance/Globalization/RegionInfo$.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Finance
+{
+    using System.Globalization;
+
+    public static class RegionInfoExtensions
+    {
+        public static bool IsUsing(this RegionInfo @this, Currency currency)
+        {
+            Require.NotNull(@this, nameof(@this));
+
+            return @this.ISOCurrencySymbol == currency.Code;
+        }
+
+        public static bool IsUsing<TCurrency>(this RegionInfo @this, TCurrency currency)
+            where TCurrency : CurrencyUnit<TCurrency>
+        {
+            Require.NotNull(@this, nameof(@this));
+
+            return @this.ISOCurrencySymbol == currency.Code;
+        }
+    }
+}
