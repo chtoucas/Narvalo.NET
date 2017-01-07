71d21858759dccffe4c748918420dc23b1487f95
Chtoucas <chtoucas@narvalo.org>
2017-01-07 08:23:15 +0100

    Explain the effect of the various rounding modes.

--- src/Narvalo.Finance/RoundingMode.cs
+++ src/Narvalo.Finance/RoundingMode.cs
@@ -4,14 +4,30 @@ namespace Narvalo.Finance
 {
     public enum RoundingMode
     {
+        /// <summary>
+        /// The number should be kept as it.
+        /// </summary>
         None,
 
+        /// <summary>
+        /// The number is already rounded.
+        /// </summary>
         Unnecessary,
 
+        /// <summary>
+        /// When a number is halfway between two others, it is rounded toward the nearest even number.
+        /// </summary>
         ToEven,
 
+        /// <summary>
+        /// When a number is halfway between two others, it is rounded toward the nearest
+        /// number that is away from zero.
+        /// </summary>
         AwayFromZero,
 
+        /// <summary>
+        /// Default IEEE 754 rounding mode.
+        /// </summary>
         Default = ToEven,
     }
 }
