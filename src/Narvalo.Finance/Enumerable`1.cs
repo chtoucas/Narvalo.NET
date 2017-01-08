9eed70de1df8d79bc9a69922323f4c112bb73b1c
Chtoucas <chtoucas@narvalo.org>
2017-01-08 06:53:52 +0100

    Add skeleton for linq extensions.

---
+++ src/Narvalo.Finance/Enumerable`1.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Finance
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Narvalo.Finance.Numerics;
+
+    public static partial class EnumerableExtensions
+    {
+        public static Money Sum(this IEnumerable<Money> @this)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Money Sum(this IEnumerable<Money> @this, MoneyRounding rounding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Money Sum(this IEnumerable<Money> @this, IDecimalRounding rounding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Money Average(this IEnumerable<Money> @this)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Money Average(this IEnumerable<Money> @this, MoneyRounding rounding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Money Average(this IEnumerable<Money> @this, IDecimalRounding rounding)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
