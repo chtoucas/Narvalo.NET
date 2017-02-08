ffdd260d458196180d36fb327bc806c206534a62
Chtoucas <chtoucas@narvalo.org>
2017-02-08 07:01:12 +0100

    Each project now has its sister test project.

---
+++ tests/Narvalo.Common.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Narvalo.Common.Facts")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("f1f1e121-3b3d-463d-8cf5-3a6e92004546")]
---
+++ tests/Narvalo.Web.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Narvalo.Web.Facts")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("49823004-daba-4fe2-b4f0-d40542af7754")]
