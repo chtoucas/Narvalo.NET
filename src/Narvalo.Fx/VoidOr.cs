bf5a9dddb60509d574b627fcc89b3f1cd4cc91aa
Chtoucas <chtoucas@narvalo.org>
2017-02-13 10:47:55 +0100

    New helper for VoidOr.

--- src/Narvalo.Fx/VoidOr.cs
+++ src/Narvalo.Fx/VoidOr.cs
@@ -3,12 +3,23 @@
 namespace Narvalo.Fx
 {
     using System;
+    using System.Runtime.ExceptionServices;
 
     /// <summary>
     /// Provides a set of static and extension methods for <see cref="VoidOr{TError}"/>.
     /// </summary>
     public partial class VoidOr
     {
+        public static void ThrowIfError(this VoidOr<ExceptionDispatchInfo> @this)
+        {
+            Require.NotNull(@this, nameof(@this));
+
+            if (@this.IsError)
+            {
+                @this.Error.Throw();
+            }
+        }
+
         public static void ThrowIfError<TException>(this VoidOr<TException> @this) where TException : Exception
         {
             Require.NotNull(@this, nameof(@this));
--- src/Narvalo.Fx/VoidOr`1.cs
+++ src/Narvalo.Fx/VoidOr`1.cs
@@ -9,6 +9,8 @@ namespace Narvalo.Fx
     using System.Runtime.CompilerServices;
 
     // Friendly version of Either<TError, Unit>.
+    // WARNING: We make it a "monad" w/ the first parameter (TError).
+    // Normally, TError should represent a light error (!). For exceptions, see VoidOrError.
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public abstract partial class VoidOr<TError> : Internal.IAlternative<TError>
     {
