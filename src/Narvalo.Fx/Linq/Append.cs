04e4a5c90d7fd1e021fd46fa359f6887607f92cb
Chtoucas <chtoucas@narvalo.org>
2017-02-17 10:56:04 +0100

    Split file AppendPrepend.cs.

--- src/Narvalo.Fx/Linq/Append.cs
+++ src/Narvalo.Fx/Linq/Append.cs
@@ -30,27 +30,5 @@ namespace Narvalo.Fx.Linq
 
             yield return element;
         }
-
-        // See remarks for Append.
-        public static IEnumerable<TSource> Prepend<TSource>(this IEnumerable<TSource> @this, TSource element)
-        {
-            Require.NotNull(@this, nameof(@this));
-            Warrant.NotNull<IEnumerable<TSource>>();
-
-            return PrependIterator(@this, element);
-        }
-
-        private static IEnumerable<TSource> PrependIterator<TSource>(IEnumerable<TSource> source, TSource element)
-        {
-            Demand.NotNull(source);
-            Warrant.NotNull<IEnumerable<TSource>>();
-
-            yield return element;
-
-            foreach (var item in source)
-            {
-                yield return item;
-            }
-        }
     }
 }
---
+++ src/Narvalo.Fx/Linq/Prepend.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Fx.Linq
+{
+    using System.Collections.Generic;
+
+    public static partial class Qperators
+    {
+        // See Append for remarks.
+        public static IEnumerable<TSource> Prepend<TSource>(this IEnumerable<TSource> @this, TSource element)
+        {
+            Require.NotNull(@this, nameof(@this));
+            Warrant.NotNull<IEnumerable<TSource>>();
+
+            return PrependIterator(@this, element);
+        }
+
+        private static IEnumerable<TSource> PrependIterator<TSource>(IEnumerable<TSource> source, TSource element)
+        {
+            Demand.NotNull(source);
+            Warrant.NotNull<IEnumerable<TSource>>();
+
+            yield return element;
+
+            foreach (var item in source)
+            {
+                yield return item;
+            }
+        }
+    }
+}
