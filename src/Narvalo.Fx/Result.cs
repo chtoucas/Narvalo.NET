53d4b0e4f1caf1393c8cf60c038eb20c1c5e0bfd
Chtoucas <chtoucas@narvalo.org>
2017-02-27 18:29:35 +0100

    Add helpers to Result.

--- src/Narvalo.Fx/Result.cs
+++ src/Narvalo.Fx/Result.cs
@@ -4,6 +4,8 @@ namespace Narvalo.Fx
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.ExceptionServices;
 
     /// <summary>
     /// Provides a set of static and extension methods for <see cref="Result{T, TError}"/>
@@ -21,6 +23,104 @@ namespace Narvalo.Fx
             if (@this.IsError)
             {
                 throw @this.Error;
+            }
+        }
+
+        public static void ThrowIfError<T>(this Result<T, ExceptionDispatchInfo> @this)
+        {
+            Require.NotNull(@this, nameof(@this));
+
+            if (@this.IsError)
+            {
+                @this.Error.Throw();
+            }
+        }
+
+        // NB: This method serves a different purpose than the trywith from F# workflows.
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "[Intentionally] Raison d'être of VoidOrError.")]
+        public static Result<Unit, ExceptionDispatchInfo> TryWith(Action action)
+        {
+            Require.NotNull(action, nameof(action));
+
+            try
+            {
+                action.Invoke();
+
+                return Of<Unit, ExceptionDispatchInfo>(Unit.Default);
+            }
+            catch (Exception ex)
+            {
+                var edi = ExceptionDispatchInfo.Capture(ex);
+
+                return FromError<Unit, ExceptionDispatchInfo>(edi);
+            }
+        }
+
+        // NB: This method is **not** the trywith from F# workflows.
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "[Intentionally] Raison d'être of ResultOrError.")]
+        public static Result<TResult, ExceptionDispatchInfo> TryWith<TResult>(Func<TResult> func)
+        {
+            Require.NotNull(func, nameof(func));
+
+            try
+            {
+                return Of<TResult, ExceptionDispatchInfo>(func.Invoke());
+            }
+            catch (Exception ex)
+            {
+                var edi = ExceptionDispatchInfo.Capture(ex);
+
+                return FromError<TResult, ExceptionDispatchInfo>(edi);
+            }
+        }
+
+        // NB: This method serves a different purpose than the tryfinally from F# workflows.
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "[Intentionally] Raison d'être of VoidOrError.")]
+        public static Result<Unit, ExceptionDispatchInfo> TryFinally(Action action, Action finallyAction)
+        {
+            Require.NotNull(action, nameof(action));
+            Require.NotNull(finallyAction, nameof(finallyAction));
+
+            try
+            {
+                action.Invoke();
+
+                return Of<Unit, ExceptionDispatchInfo>(Unit.Default);
+            }
+            catch (Exception ex)
+            {
+                var edi = ExceptionDispatchInfo.Capture(ex);
+
+                return FromError<Unit, ExceptionDispatchInfo>(edi);
+            }
+            finally
+            {
+                finallyAction.Invoke();
+            }
+        }
+
+        // NB: This method is **not** the tryfinally from F# workflows.
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "[Intentionally] Raison d'être of ResultOrError.")]
+        public static Result<TResult, ExceptionDispatchInfo> TryFinally<TResult>(
+            Func<TResult> func,
+            Action finallyAction)
+        {
+            Require.NotNull(func, nameof(func));
+            Require.NotNull(finallyAction, nameof(finallyAction));
+
+            try
+            {
+                return Of<TResult, ExceptionDispatchInfo>(func.Invoke());
+            }
+            catch (Exception ex)
+            {
+                var edi = ExceptionDispatchInfo.Capture(ex);
+
+                return FromError<TResult, ExceptionDispatchInfo>(edi);
+            }
+            finally
+            {
+                finallyAction.Invoke();
             }
         }
     }
