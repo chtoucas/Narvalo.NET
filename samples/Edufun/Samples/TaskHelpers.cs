4210562e6664300c38d6ebb8d9bb56588a03bada
Chtoucas <chtoucas@narvalo.org>
2017-03-08 07:30:52 +0100

    More samples for Task.

--- samples/Edufun/Samples/TaskHelpers.cs
+++ samples/Edufun/Samples/TaskHelpers.cs
@@ -6,11 +6,12 @@ namespace Edufun.Samples
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    // Adapted from https://blogs.msdn.microsoft.com/pfxteam/2013/04/03/tasks-monads-and-linq/
-    // See also:
+    // Adapted/copied from:
+    // - https://blogs.msdn.microsoft.com/pfxteam/2013/04/03/tasks-monads-and-linq/
     // - https://blogs.msdn.microsoft.com/pfxteam/2012/08/15/implementing-then-with-await/
+    // See also:
     // - https://blogs.msdn.microsoft.com/pfxteam/2010/11/21/processing-sequences-of-asynchronous-operations-with-tasks/
-    public static class TaskExtensions
+    public static class TaskHelpers
     {
         #region Monad
 
@@ -38,26 +39,67 @@ namespace Edufun.Samples
 
         #endregion
 
+        #region Monad extensions
+
+        public static async Task<TResult> ContinueWith<TSource, TResult>(Task<TSource> source, Task<TResult> other)
+        {
+            // > return await source.Bind(_ => other);
+
+            await source;
+            return await other;
+        }
+
+        public static async Task<TSource> PassBy<TSource, TOther>(
+            Task<TSource> source,
+            Task<TOther> other)
+        {
+            // > return await source.Zip(other, (arg, _) => arg);
+
+            TSource val = await source;
+            await other;
+
+            return val;
+        }
+
+        public static async Task<TResult> Zip<T1, T2, TResult>(
+            Task<T1> first,
+            Task<T2> second,
+            Func<T1, T2, TResult> zipper)
+        {
+            // > return await first.Bind(
+            // >     arg1 => second.Select(
+            // >         arg2 => zipper(arg1, arg2)));
+
+            T1 val1 = await first;
+            T2 val2 = await second;
+
+            return zipper(val1, val2);
+        }
+
+        #endregion
+
         #region Query Expresion Pattern
 
         public static async Task<TResult> Select<TSource, TResult>(
             this Task<TSource> @this,
             Func<TSource, TResult> selector)
         {
-            TSource val = await @this;
+            // > return await @this.Bind(val => Return(selector(val)));
 
-            return selector(val);
+            TSource source = await @this;
+
+            return selector(source);
         }
 
         public static async Task<TSource> Where<TSource>(
             this Task<TSource> @this,
             Func<TSource, bool> predicate)
         {
-            TSource val = await @this;
+            TSource source = await @this;
 
-            if (!predicate(val)) { throw new OperationCanceledException(); }
+            if (!predicate(source)) { throw new OperationCanceledException(); }
 
-            return val;
+            return source;
         }
 
         public static async Task<TResult> SelectMany<TSource, TMiddle, TResult>(
@@ -65,10 +107,10 @@ namespace Edufun.Samples
             Func<TSource, Task<TMiddle>> selector,
             Func<TSource, TMiddle, TResult> resultSelector)
         {
-            TSource val = await @this;
-            TMiddle middle = await selector(val);
+            TSource source = await @this;
+            TMiddle middle = await selector(source);
 
-            return resultSelector(val, middle);
+            return resultSelector(source, middle);
         }
 
         public static async Task<TResult> Join<TSource, TInner, TKey, TResult>(
@@ -98,11 +140,11 @@ namespace Edufun.Samples
             Func<TInner, TKey> innerKeySelector,
             Func<TSource, Task<TInner>, TResult> resultSelector)
         {
-            TSource val = await @this;
+            TSource source = await @this;
 
-            return resultSelector(val,
+            return resultSelector(source,
                 inner.Where(
-                    inval => EqualityComparer<TKey>.Default.Equals(outerKeySelector(val), innerKeySelector(inval))));
+                    inval => EqualityComparer<TKey>.Default.Equals(outerKeySelector(source), innerKeySelector(inval))));
         }
 
         //public static async Task<TSource> Cast<TSource>(this Task source)
@@ -111,6 +153,46 @@ namespace Edufun.Samples
 
         //    return (TSource)((dynamic)source).Result;
         //}
+
+        #endregion
+
+        #region Then()
+
+        public static async Task Then(this Task source, Action next)
+        {
+            await source;
+            next();
+        }
+
+        public static async Task<TResult> Then<TResult>(this Task source, Func<TResult> next)
+        {
+            await source;
+            return next();
+        }
+
+        public static async Task Then(this Task source, Func<Task> next)
+        {
+            await source;
+            await next();
+        }
+
+        public static async Task<TResult> Then<TResult>(this Task source, Func<Task<TResult>> next)
+        {
+            await source;
+            return await next();
+        }
+
+        public static async Task Then<TSource>(this Task<TSource> source, Action<TSource> next)
+            => next(await source);
+
+        public static async Task<TResult> Then<TSource, TResult>(this Task<TSource> source, Func<TSource, TResult> next)
+            => next(await source);
+
+        public static async Task Then<TSource>(this Task<TSource> source, Func<TSource, Task> next)
+            => await next(await source);
+
+        public static async Task<TResult> Then<TSource, TResult>(this Task<TSource> source, Func<TSource, Task<TResult>> next)
+            => await next(await source);
 
         #endregion
     }
