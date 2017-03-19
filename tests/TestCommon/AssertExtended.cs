7393eec0e1322c0b58d1806271969dcf689e6495
Chtoucas <chtoucas@narvalo.org>
2017-03-19 11:13:59 +0100

    Code style fix.

--- tests/TestCommon/AssertExtended.cs
+++ tests/TestCommon/AssertExtended.cs
@@ -6,13 +6,15 @@ namespace Narvalo
     using System.Globalization;
     using System.Resources;
 
+    using Xunit;
+
     public partial class AssertExtended : Xunit.Assert
     {
         public static void IsNotLocalized(LocalizedStrings localizedStrings)
         {
             var dict = localizedStrings.GetStrings();
 
-            Null(dict);
+            Assert.Null(dict);
         }
 
         public static void IsLocalized(ResourceManager manager)
@@ -22,15 +24,15 @@ namespace Narvalo
         {
             var dict = localizedStrings.GetStrings();
 
-            NotNull(dict);
+            Assert.NotNull(dict);
 
-            All(dict, pair =>
-                True(!String.IsNullOrWhiteSpace(pair.Value),
+            Assert.All(dict, pair =>
+                Assert.True(!String.IsNullOrWhiteSpace(pair.Value),
                        $"The resource '{pair.Key}' is empty or contains only white spaces.")
             );
 
-            All(dict, pair =>
-                True(pair.Value != "XXX",
+            Assert.All(dict, pair =>
+                Assert.True(pair.Value != "XXX",
                     $"The key '{pair.Key}' contains a temporary string in {CultureInfo.CurrentCulture.EnglishName}.")
             );
         }
@@ -40,26 +42,26 @@ namespace Narvalo
             var dict = localizedStrings.GetStrings();
             if (dict == null)
             {
-                True(false, $"No localized strings found in {CultureInfo.CurrentCulture.EnglishName}.");
+                Assert.True(false, $"No localized strings found in {CultureInfo.CurrentCulture.EnglishName}.");
                 return;
             }
 
             var keys = localizedStrings.ReferenceKeys;
             if (keys == null)
             {
-                True(false, $"Unable to load the reference keys.");
+                Assert.True(false, $"Unable to load the reference keys.");
                 return;
             }
 
             foreach (var pair in dict)
             {
-                True(keys.Contains(pair.Key),
+                Assert.True(keys.Contains(pair.Key),
                     $"The resource contains an unrecognized key '{pair.Key}' in {CultureInfo.CurrentCulture.EnglishName}.");
             }
 
             foreach (var key in keys)
             {
-                True(dict.ContainsKey(key),
+                Assert.True(dict.ContainsKey(key),
                     $"The key '{key}' does not exist in {CultureInfo.CurrentCulture.EnglishName}.");
             }
         }
