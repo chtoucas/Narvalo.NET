c61455f2c169a22fd0d3dd2258e39b59dddc2334
Chtoucas <chtoucas@narvalo.org>
2017-03-19 13:15:40 +0100

    Add tests for Format and HashCodeHelpers.

--- tests/Narvalo.Core.Facts/FormatFacts.cs
+++ tests/Narvalo.Core.Facts/FormatFacts.cs
@@ -11,40 +11,80 @@ namespace Narvalo
         #region Current()
 
         [Fact]
-        public static void Current_ThrowsArgumentNullException_ForNullFormat()
-            => Assert.Throws<ArgumentNullException>(() => Format.Current(null));
+        public static void Current1_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Current(null, 1));
 
         [Fact]
-        public static void Current_ThrowsArgumentNullException_ForNullArgs()
-            => Assert.Throws<ArgumentNullException>(() => Format.Current(String.Empty, null));
+        public static void Current2_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Current(null, 1, 2));
 
         [Fact]
-        public static void Current_ThrowsFormatException_ForInvalidFormat()
-            => Assert.Throws<FormatException>(() => Format.Current("{0}"));
+        public static void Current3_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Current(null, 1, 2, 3));
 
         [Fact]
-        public static void Current_ReturnsNonNull()
-            => Assert.NotNull(Format.Current(String.Empty));
+        public static void Current1_DoesNotThrow_ForNullArgs()
+            => Format.Current("{0}", (string)null);
+
+        [Fact]
+        public static void Current2_DoesNotThrow_ForNullArgs()
+            => Format.Current("{0}{1}", (string)null, (string)null);
+
+        [Fact]
+        public static void Current3_DoesNotThrow_ForNullArgs()
+            => Format.Current("{0}{1}{2}", (string)null, (string)null, (string)null);
+
+        [Fact]
+        public static void Current1_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Current("{0} {1}", 1));
+
+        [Fact]
+        public static void Current2_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Current("{0} {1} {2}", 1, 2));
+
+        [Fact]
+        public static void Current3_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Current("{0} {1} {2} {3}", 1, 2, 3));
 
         #endregion
 
         #region Invariant()
 
         [Fact]
-        public static void Invariant_ThrowsArgumentNullException_ForNullFormat()
-            => Assert.Throws<ArgumentNullException>(() => Format.Invariant(null));
+        public static void Invariant1_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Invariant(null, 1));
 
         [Fact]
-        public static void Invariant_ThrowsArgumentNullException_ForNullArgs()
-            => Assert.Throws<ArgumentNullException>(() => Format.Invariant(String.Empty, null));
+        public static void Invariant2_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Invariant(null, 1, 2));
 
         [Fact]
-        public static void Invariant_ThrowsFormatException_ForInvalidFormat()
-            => Assert.Throws<FormatException>(() => Format.Invariant("{0}"));
+        public static void Invariant3_ThrowsArgumentNullException_ForNullFormat()
+            => Assert.Throws<ArgumentNullException>(() => Format.Invariant(null, 1, 2, 3));
 
         [Fact]
-        public static void Invariant_ReturnsNonNull()
-            => Assert.NotNull(Format.Invariant(String.Empty));
+        public static void Invariant1_DoesNotThrow_ForNullArgs()
+            => Format.Invariant("{0}", (string)null);
+
+        [Fact]
+        public static void Invariant2_DoesNotThrow_ForNullArgs()
+            => Format.Invariant("{0}{1}", (string)null, (string)null);
+
+        [Fact]
+        public static void Invariant3_DoesNotThrow_ForNullArgs()
+            => Format.Invariant("{0}{1}{2}", (string)null, (string)null, (string)null);
+
+        [Fact]
+        public static void Invariant1_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Invariant("{0} {1}", 1));
+
+        [Fact]
+        public static void Invariant2_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Invariant("{0} {1} {2}", 1, 2));
+
+        [Fact]
+        public static void Invariant3_ThrowsFormatException_ForInvalidFormat()
+            => Assert.Throws<FormatException>(() => Format.Invariant("{0} {1} {2} {3}", 1, 2, 3));
 
         #endregion
     }
---
+++ tests/Narvalo.Core.Facts/HashCodeHelpersFacts.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo
+{
+    using System;
+
+    using Xunit;
+
+    public static class HashCodeHelpersFacts
+    {
+        [Fact]
+        public static void Combine2_DoesNotThrow_WhenOverflowed()
+            => HashCodeHelpers.Combine(Int32.MaxValue, 1);
+
+        [Fact]
+        public static void Combine3_DoesNotThrow_WhenOverflowed()
+            => HashCodeHelpers.Combine(Int32.MaxValue, 1, 1);
+    }
+}
