74f059804f90b14035a8f4fba00fe1abfd95b4fa
Chtoucas <chtoucas@narvalo.org>
2017-03-29 11:22:21 +0200

    Add a test for Recursion.

---
+++ tests/Narvalo.Fx.Facts/Applicative/RecursionFacts.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Applicative {
+    using System;
+
+    using Xunit;
+
+    public static class RecursionFacts {
+        internal sealed class tAttribute : TestCaseAttribute {
+            public tAttribute(string description) : base(nameof(Recursion), description) { }
+        }
+
+        [t("Fix() guards.")]
+        public static void Fix0() {
+            Assert.Throws<ArgumentNullException>("generator", () => Recursion.Fix(default(Func<Func<int, int>, Func<int, int>>)));
+        }
+    }
+}
