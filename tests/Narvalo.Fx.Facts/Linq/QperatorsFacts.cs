a4337671b2c20e38d9a9dcb959316dc00cfc4d7b
Chtoucas <chtoucas@narvalo.org>
2017-03-29 18:04:22 +0200

    Cleanup.

--- tests/Narvalo.Fx.Facts/Linq/QperatorsFacts.cs
+++ tests/Narvalo.Fx.Facts/Linq/QperatorsFacts.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
 
 namespace Narvalo.Linq {
+    using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq.Tests;
 
@@ -15,6 +17,16 @@ namespace Narvalo.Linq {
 
         private static IEnumerable<T> EmptySource<T>() {
             yield break;
+        }
+
+        /// <summary>
+        /// Class to help for deferred execution tests: it throw an exception
+        /// if GetEnumerator is called.
+        /// </summary>
+        private sealed class ThrowingEnumerable<T> : IEnumerable<T> {
+            public IEnumerator<T> GetEnumerator() => throw new InvalidOperationException();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
     }
 }
