7dab305767f6c15e05de98a061b2966590962a16
Chtoucas <chtoucas@narvalo.org>
2017-04-04 10:44:08 +0200

    Unit imlpements IEquatable<ValueTuple>.

--- src/Narvalo.Fx/Applicative/Unit.cs
+++ src/Narvalo.Fx/Applicative/Unit.cs
@@ -5,7 +5,7 @@ namespace Narvalo.Applicative
     using System;
     using System.Diagnostics.CodeAnalysis;
 
-    public struct Unit : IEquatable<Unit>
+    public struct Unit : IEquatable<Unit>, IEquatable<ValueTuple>
     {
         private static readonly Unit s_Default = new Unit();
 
@@ -19,9 +19,27 @@ namespace Narvalo.Applicative
         [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "right", Justification = "[Intentionally] This method always returns 'false'.")]
         public static bool operator !=(Unit left, Unit right) => false;
 
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "left", Justification = "[Intentionally] This method always returns 'true'.")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "right", Justification = "[Intentionally] This method always returns 'true'.")]
+        public static bool operator ==(Unit left, ValueTuple right) => true;
+
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "left", Justification = "[Intentionally] This method always returns 'false'.")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "right", Justification = "[Intentionally] This method always returns 'false'.")]
+        public static bool operator !=(Unit left, ValueTuple right) => false;
+
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "left", Justification = "[Intentionally] This method always returns 'true'.")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "right", Justification = "[Intentionally] This method always returns 'true'.")]
+        public static bool operator ==(ValueTuple left, Unit right) => true;
+
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "left", Justification = "[Intentionally] This method always returns 'false'.")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "right", Justification = "[Intentionally] This method always returns 'false'.")]
+        public static bool operator !=(ValueTuple left, Unit right) => false;
+
         public bool Equals(Unit other) => true;
 
-        public override bool Equals(object obj) => obj is Unit;
+        public bool Equals(ValueTuple other) => true;
+
+        public override bool Equals(object obj) => obj is Unit || obj is ValueTuple;
 
         public override int GetHashCode() => 0;
 
--- tests/Narvalo.Fx.Facts/Applicative/UnitFacts.cs
+++ tests/Narvalo.Fx.Facts/Applicative/UnitFacts.cs
@@ -10,26 +10,36 @@ namespace Narvalo.Applicative {
             public tAttribute(string description) : base(nameof(Unit), description) { }
         }
 
-        [t("== is always true.")]
+        [t("== and !=.")]
         public static void Equality1() {
             var u = Unit.Default;
             var u1 = new Unit();
             var u2 = new Unit();
 
-            Assert.True(u1 == u);
             Assert.True(u == u1);
+            Assert.True(u1 == u);
             Assert.True(u1 == u2);
+
+            Assert.False(u != u1);
+            Assert.False(u1 != u);
+            Assert.False(u1 != u2);
         }
 
-        [t("!= is always false.")]
-        public static void Inequality1() {
+        [t("== and != w/ ().")]
+        public static void Equality2() {
             var u = Unit.Default;
             var u1 = new Unit();
-            var u2 = new Unit();
+            var t = new ValueTuple();
 
-            Assert.False(u1 != u);
-            Assert.False(u != u1);
-            Assert.False(u1 != u2);
+            Assert.True(u == t);
+            Assert.True(t == u);
+            Assert.True(u1 == t);
+            Assert.True(t == u1);
+
+            Assert.False(u != t);
+            Assert.False(t != u);
+            Assert.False(u1 != t);
+            Assert.False(t != u1);
         }
 
         [t("Equals(unit) is always true.")]
@@ -38,16 +48,30 @@ namespace Narvalo.Applicative {
             var u1 = new Unit();
             var u2 = new Unit();
 
-            Assert.True(u1.Equals(u1));
-            Assert.True(u1.Equals(u2));
-            Assert.True(u1.Equals(u));
+            Assert.True(u.Equals(u));
             Assert.True(u.Equals(u1));
             Assert.True(u.Equals(u2));
-            Assert.True(u.Equals(u));
+            Assert.True(u1.Equals(u));
+            Assert.True(u1.Equals(u1));
+            Assert.True(u1.Equals(u2));
         }
 
-        [t("Equals(non-unit) is always false.")]
+        [t("Equals(ValueTuple) is always true.")]
         public static void Equals2() {
+            var u = Unit.Default;
+            var u1 = new Unit();
+            var t1 = new ValueTuple();
+            // Boxed version.
+            object t2 = new ValueTuple();
+
+            Assert.True(u.Equals(t1));
+            Assert.True(u.Equals(t2));
+            Assert.True(u1.Equals(t1));
+            Assert.True(u1.Equals(t2));
+        }
+
+        [t("Equals(non-unit or ()) is always false.")]
+        public static void Equals3() {
             var u = Unit.Default;
             var u1 = new Unit();
 
@@ -66,8 +90,8 @@ namespace Narvalo.Applicative {
             var u = Unit.Default;
             var u1 = new Unit();
 
-            Assert.Equal(0, u1.GetHashCode());
             Assert.Equal(0, u.GetHashCode());
+            Assert.Equal(0, u1.GetHashCode());
         }
 
         [t("ToString() always returns ().")]
@@ -75,8 +99,8 @@ namespace Narvalo.Applicative {
             var u = Unit.Default;
             var u1 = new Unit();
 
-            Assert.Equal("()", u1.ToString());
             Assert.Equal("()", u.ToString());
+            Assert.Equal("()", u1.ToString());
         }
     }
 }
