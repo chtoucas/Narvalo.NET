4ea697b4503e7de6ca0e66d5b3dbf8896d3ebaa8
Chtoucas <chtoucas@narvalo.org>
2017-04-10 09:05:26 +0200

    Add operator None.

---
+++ src/Narvalo.Fx/Linq/None.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Linq
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static partial class Qperators
+    {
+        // Returns true if no element in the sequence satisfies the predicate; otherwise false.
+        public static bool None<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            Require.NotNull(source, nameof(source));
+            Require.NotNull(predicate, nameof(predicate));
+
+            // Same as !source.Any(predicate);
+            // Same as source.All(x => !predicate(x));
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
