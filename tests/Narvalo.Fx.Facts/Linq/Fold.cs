6b525a9b194e3f1b5ea08ff3cab6cedf4ed6140a
Chtoucas <chtoucas@narvalo.org>
2017-04-10 09:07:47 +0200

    Cleanup.

--- tests/Narvalo.Fx.Facts/Linq/Fold.cs
+++ tests/Narvalo.Fx.Facts/Linq/Fold.cs
@@ -8,6 +8,21 @@ namespace Narvalo.Linq {
     using Narvalo.Applicative;
     using Xunit;
 
+    public partial class QperatorsFacts {
+        [t("Fold() guards.")]
+        public static void Fold0() {
+            Func<int, int, int> accumulator = (i, j) => i + j;
+            Func<int, bool> predicate = _ => true;
+
+            IEnumerable<int> nil = null;
+            Assert.Throws<ArgumentNullException>("source", () => nil.Fold(1, accumulator, predicate));
+
+            var source = Enumerable.Range(0, 1);
+            Assert.Throws<ArgumentNullException>("accumulator", () => source.Fold(1, null, predicate));
+            Assert.Throws<ArgumentNullException>("predicate", () => source.Fold(1, accumulator, null));
+        }
+    }
+
     // Fold for Either.
     public partial class QperatorsFacts {
         [t("Fold() guards (Either).")]
