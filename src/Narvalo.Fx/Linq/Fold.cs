9cc7a548f7a5cfca3c98d6cee4eb62a42b516951
Chtoucas <chtoucas@narvalo.org>
2017-04-10 09:06:06 +0200

    Give Reduce and Fold their own file.

---
+++ src/Narvalo.Fx/Linq/Fold.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Narvalo.Org. All rights reserved. See LICENSE.txt in the project root for license information.
+
+namespace Narvalo.Linq
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static partial class Qperators
+    {
+        // Strictly equivalent to Enumerable.Aggregate. Added for symmetry.
+        public static TAccumulate Fold<TSource, TAccumulate>(
+            this IEnumerable<TSource> source,
+            TAccumulate seed,
+            Func<TAccumulate, TSource, TAccumulate> accumulator)
+        {
+            Require.NotNull(source, nameof(source));
+            Require.NotNull(accumulator, nameof(accumulator));
+
+            TAccumulate retval = seed;
+
+            using (var iter = source.GetEnumerator())
+            {
+                while (iter.MoveNext())
+                {
+                    retval = accumulator(retval, iter.Current);
+                }
+            }
+
+            return retval;
+        }
+
+        public static TAccumulate Fold<TSource, TAccumulate>(
+            this IEnumerable<TSource> source,
+            TAccumulate seed,
+            Func<TAccumulate, TSource, TAccumulate> accumulator,
+            Func<TAccumulate, bool> predicate)
+        {
+            Require.NotNull(source, nameof(source));
+            Require.NotNull(accumulator, nameof(accumulator));
+            Require.NotNull(predicate, nameof(predicate));
+
+            TAccumulate retval = seed;
+
+            using (var iter = source.GetEnumerator())
+            {
+                while (predicate(retval) && iter.MoveNext())
+                {
+                    retval = accumulator(retval, iter.Current);
+                }
+            }
+
+            return retval;
+        }
+    }
+}
--- src/Narvalo.Fx/Linq/Reduce.cs
+++ src/Narvalo.Fx/Linq/Reduce.cs
@@ -10,51 +10,6 @@ namespace Narvalo.Linq
     public static partial class Qperators
     {
         // Strictly equivalent to Enumerable.Aggregate. Added for symmetry.
-        public static TAccumulate Fold<TSource, TAccumulate>(
-            this IEnumerable<TSource> source,
-            TAccumulate seed,
-            Func<TAccumulate, TSource, TAccumulate> accumulator)
-        {
-            Require.NotNull(source, nameof(source));
-            Require.NotNull(accumulator, nameof(accumulator));
-
-            TAccumulate retval = seed;
-
-            using (var iter = source.GetEnumerator())
-            {
-                while (iter.MoveNext())
-                {
-                    retval = accumulator(retval, iter.Current);
-                }
-            }
-
-            return retval;
-        }
-
-        public static TAccumulate Fold<TSource, TAccumulate>(
-            this IEnumerable<TSource> source,
-            TAccumulate seed,
-            Func<TAccumulate, TSource, TAccumulate> accumulator,
-            Func<TAccumulate, bool> predicate)
-        {
-            Require.NotNull(source, nameof(source));
-            Require.NotNull(accumulator, nameof(accumulator));
-            Require.NotNull(predicate, nameof(predicate));
-
-            TAccumulate retval = seed;
-
-            using (var iter = source.GetEnumerator())
-            {
-                while (predicate(retval) && iter.MoveNext())
-                {
-                    retval = accumulator(retval, iter.Current);
-                }
-            }
-
-            return retval;
-        }
-
-        // Strictly equivalent to Enumerable.Aggregate. Added for symmetry.
         public static TSource Reduce<TSource>(
             this IEnumerable<TSource> source,
             Func<TSource, TSource, TSource> accumulator)
